31369fcf2a9b9971519f5b5858c6aeaf53df7504
EmmanuelK <EmmanuelK@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2008-11-07 15:29:55 +0000

    create new java package "actions"

---
+++ CHOX_API/src/chox/services/ClaimService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chox.services;
+
+import chox.models.Claim;
+import java.util.List;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public interface ClaimService {
+    
+    public List<Claim> getAllClaims();
+
+}
---
+++ CHOX_API/src/chox/services/ClaimServiceImpl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chox.services;
+
+import chox.models.Claim;
+import java.util.List;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class ClaimServiceImpl implements ClaimService {
+    
+    public List<Claim> getAllClaims()
+    {
+        return null;
+    }    
+    
+}
