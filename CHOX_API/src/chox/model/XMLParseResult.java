bd9dde7ecc87c63bc462f4835fcdfb254756369b
carlsonh <carlsonh@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2008-11-18 21:14:51 +0000

    Carlson's copy, last update at 18 Nov 2008

--- CHOX_API/src/chox/model/XMLParseResult.java
+++ CHOX_API/src/chox/model/XMLParseResult.java
@@ -27,7 +27,16 @@ public class XMLParseResult {
     private ArrayList<Witness> witnesses;
     private ArrayList<Injury> injuries;
     private ArrayList<Solicitor> solicitors;
+    private ArrayList<EngineerReport> engineerReports;
 
+    public ArrayList<EngineerReport> getEngineerReports() {
+        return engineerReports;
+    }
+
+    public void setEngineerReports(ArrayList<EngineerReport> engineerReports) {
+        this.engineerReports = engineerReports;
+    }
+    
     public ArrayList<Injury> getInjuries() {
         return injuries;
     }
