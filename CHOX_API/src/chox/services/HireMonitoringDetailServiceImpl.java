bdd9a1fbf900ae91ff1c7eab0efa661e4dad9308
carlsonh <carlsonh@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2008-11-21 12:55:13 +0000

    Carlson's copy, last update at 21 Nov 2008

---
+++ CHOX_API/src/chox/services/HireMonitoringDetailServiceImpl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chox.services;
+
+import chox.model.HireMonitoringDetail;
+        
+public class HireMonitoringDetailServiceImpl {
+
+    public HireMonitoringDetail getHireMonitoringDetailByVehicleHireId(int vehiclehireId){
+        
+        HireMonitoringDetail hiremonitoringdetail = new HireMonitoringDetail();
+        
+        return hiremonitoringdetail;
+    }
+}
