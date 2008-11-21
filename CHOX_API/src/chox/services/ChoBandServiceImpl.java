6dbf126bb6000073f786910dfe11eb633b676c07
carlsonh <carlsonh@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2008-11-21 12:54:53 +0000

    Carlson's copy, last update at 21 Nov 2008

---
+++ CHOX_API/src/chox/services/ChoBandServiceImpl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chox.services;
+
+import chox.model.ChoBand;
+
+public class ChoBandServiceImpl {
+
+    public ChoBand getChoBandByChorganisationId(int Id){
+        
+        
+        //IChoBandInfo band = new ChoBand(); 
+        //return band;
+        return new ChoBand();
+    }
+}
