71518b76749853785772bc148a3751874c1614bc
carlsonh <carlsonh@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2009-07-08 09:35:57 +0000

    Not in Use. Created by Emm at 2008 June

--- CHOX/src/chox/decision/Decision.java
+++
@@ -1,8 +0,0 @@
-package chox.decision;
-
-import chox.data.Rental;
-import com.filesystemsoftware.utils.DBConnectionWrapper;
-
-public abstract class Decision {
-    public abstract Boolean decide(DBConnectionWrapper connection,Rental rental) throws Exception;    
-}
