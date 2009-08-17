50de069df72f2fd63bf4d34b46e95327e15875bf
carlsonh <carlsonh@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2009-08-17 13:15:12 +0000

    Version: V2.7 Description: Branch Project

---
+++ breEngine/src/scsbre/engine/rules/HasCorrectRepairGrossCalculation.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scsbre.engine.rules;
+
+import scsbre.engine.IBusinessRule;
+import scsbre.engine.RuleEvaluation;
+import scsbre.engine.RuleEvaluationResult;
+import scsbre.engine.util.CalcHelper;
+import scsbre.engine.util.InvoiceCalcHelper;
+import scsbre.model.ClaimStatus;
+import scsbre.model.IClaimInfo;
+import scsbre.model.IInvoiceInfo;
+
+/**
+ *
+ * @author Derm
+ * 
+ * rule 11, order 19
+ */
+public class HasCorrectRepairGrossCalculation implements IBusinessRule {
+
+
+    
+    public RuleEvaluation applyToClaim(IClaimInfo claim) {
+        
+        RuleEvaluation res = new RuleEvaluation();
+        
+        IInvoiceInfo invoice = claim.getInvoice();
+        InvoiceCalcHelper iCalc = InvoiceCalcHelper.getInstance(invoice);
+        boolean success = CalcHelper.EqualTo(invoice.getRepairGross(), iCalc.getCalculatedRepairGross());
+        
+        res.setResult(success ? RuleEvaluationResult.RulePassed : RuleEvaluationResult.RuleFailed);
+        res.setIsVisibleToCHO(true);
+        res.setRelatedRule(this);
+        
+        return res;
+
+    }     
+
+    public String getNarrative() {
+        return "Repair Gross calculation is incorrect.";
+    }
+
+    public String getRuleId() {
+        return "011";
+    }
+    
+    public ClaimStatus getStatusAfterFailure() {
+        return ClaimStatus.InvoiceDataCalculationIncorrect;
+    }    
+
+}
