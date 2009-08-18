0a93a29726379008de922b3a0978a45ef20a1a39
carlsonh <carlsonh@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2009-08-18 10:07:41 +0000

    Version: V2.7 Description: Branch Project

---
+++ JobScheduler/src/jobscheduler/services/DataService.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jobscheduler.services;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class DataService {
+
+    
+    final String databaseServer = "localhost:5432";
+    final String databaseConnection = "chox";
+    final String databaseUserName = "chox";
+    final String databaseUserPsw= "chox";
+    
+    /*
+    final String databaseServer = "10.0.0.2:5432";
+    final String databaseConnection = "chox_test";
+    final String databaseUserName = "postgres";
+    final String databaseUserPsw= "GreenF1nCH";
+    */
+    
+    public Connection getConnection(){
+
+        Connection c = null;
+        
+        try {
+            
+            if(isDriverExist()){
+                c = DriverManager.getConnection("jdbc:postgresql://"+databaseServer+"/" + databaseConnection, databaseUserName, databaseUserPsw);
+            }
+            
+        } catch (SQLException se) {
+            System.out.println("Couldn't connect: print out a stack trace and exit.");
+            se.printStackTrace();
+            System.exit(1);
+        }
+
+        return c;
+    }
+    
+    private boolean isDriverExist(){
+
+        boolean bFlag = true;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            
+        } catch (ClassNotFoundException cnfe) {
+            bFlag = false;
+            cnfe.printStackTrace();
+            System.exit(1);
+        }
+        
+        return bFlag;
+    }
+}
