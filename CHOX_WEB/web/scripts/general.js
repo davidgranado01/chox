b3865ddbd4211b7b1e57171a8284bf67882bf55c
carlsonh <carlsonh@afc7a831-a588-449f-b2b6-ab20b6eddc6b>
2009-12-18 20:59:26 +0000

    Sptinrg8 ENhancment

--- CHOX_WEB/web/scripts/general.js
+++ CHOX_WEB/web/scripts/general.js
@@ -106,7 +106,7 @@ function onOpenAbout(){
         
     var msg = "<span class='aboutProductName'>Product Name: iDAS CHOX</span><br/><br/>";
     msg = msg + "<span class='acountCopyright'>Copyright Message: ©2009 Sherwood Compliance Services Ltd</span><br/><br/>";
-    msg = msg + "<span class='acountVersionNumber'>V2.8.2.34 - 20091201</span><br/><br/>";
+    msg = msg + "<span class='acountVersionNumber'>V2.8.2.36 - 20091218</span><br/><br/>";
     msg = msg + "<input type='button' value='Close' onclick='javascript:$.unblockUI();'>";
 
     $.blockUI({
