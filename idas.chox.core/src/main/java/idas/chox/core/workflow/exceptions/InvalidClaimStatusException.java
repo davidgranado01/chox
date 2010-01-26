09b290328e6592635aaf552697d90ceb6a5d1f02
Carlson <Carlson@10.0.0.6>
2010-01-26 09:59:41 +0000

    TEST COMMIT
    Signed-off-by: Abrar Malang <abrar.malang@sherwoodcompliance.co.uk>

--- idas.chox.core/src/main/java/idas/chox/core/workflow/exceptions/InvalidClaimStatusException.java
+++ idas.chox.core/src/main/java/idas/chox/core/workflow/exceptions/InvalidClaimStatusException.java
@@ -13,6 +13,8 @@ import idas.chox.core.model.Claim;
 public class InvalidClaimStatusException extends Exception {
 
     public InvalidClaimStatusException(Claim claim) {
-        super(String.format("Attemp to process claim with id : %d, but claim status : '%s' is invalid.",claim.getId(),claim.getStatus()));
+        // 16 Bug Id:79
+        // super(String.format("Attemp to process claim with id : %d, but claim status : '%s' is invalid.",claim.getId(),claim.getStatus()));
+        super(String.format("Unable to process claim. The claim has been processed by another user. Please see the 'Claim Cycle' tab for further details"));
     }
 }
