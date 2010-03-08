eb91ab604c9d0bc43e8ca9da71bb55116c10f5c7
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2010-03-08 11:58:23 +0000

    Bug#127 fixed (Hire Start & End Reconciliation Check problem). This was previously reported as mantis bug#0000912 and the same patch has been applied.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/bre/util/CalcHelper.java

--- idas.chox.service/src/main/java/idas/chox/service/bre/util/CalcHelper.java
+++ idas.chox.service/src/main/java/idas/chox/service/bre/util/CalcHelper.java
@@ -1,5 +1,6 @@
 package idas.chox.service.bre.util;
 
+import idas.chox.core.util.DateHelper;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -43,7 +44,9 @@ public class CalcHelper {
     private static final long MILISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
 
     public static int getDaysBetweenDates(Date startDate, Date endDate) {
-        long diff = endDate.getTime() - startDate.getTime();
+        // Mantis Id 0000912
+        long diff = DateHelper.setDateOnly(endDate).getTime() - DateHelper.setDateOnly(startDate).getTime();
+        // long diff = endDate.getTime() - startDate.getTime();
         int days = (int) Math.floor(diff / MILISECONDS_PER_DAY);
         return Math.abs(days); //add an extra day to factor "part thereof" rule
     }
