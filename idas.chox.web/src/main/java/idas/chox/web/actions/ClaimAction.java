7bb666dd15eaba8c2e518e65bff5626295ddff82
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2010-04-14 16:04:31 +0100

    Added revert option for Insurer with claim status 'ClaimReferredToEngineer' (>> ClaimUnacknowledgedRouted, ClaimPending, ClaimRejectionContested, ClaimUpdatedByEngineer).
    	modified:   idas.chox.web/src/main/java/idas/chox/web/actions/ClaimAction.java

--- idas.chox.web/src/main/java/idas/chox/web/actions/ClaimAction.java
+++ idas.chox.web/src/main/java/idas/chox/web/actions/ClaimAction.java
@@ -321,6 +321,7 @@ public class ClaimAction extends BaseAction implements ModelDriven<Claim>, Prepa
         if ((getIsCHO() || getIsChoxAdmin()) && claim.getStatus().equals("AwaitingInvoiceData"))
             result = true;
         else if ((getIsInsurer() || getIsChoxAdmin()) && (claim.getStatus().equals("ClaimReferredToFNOL")
+                                    || claim.getStatus().equals("ClaimReferredToEngineer")
                                     || claim.getStatus().equals("InvoiceReferredToEngineer")
                                     || claim.getStatus().equals("InvoicePaymentLogged")))
             result = true;
