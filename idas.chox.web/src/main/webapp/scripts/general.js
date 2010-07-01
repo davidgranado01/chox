4e9fd3f5690dd138a85c9a003c9203f57cc08710
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2010-07-01 11:25:28 +0100

    Updated bordereau schema macro-version number to 2.7.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/xml/validations/BordereauSchemaValidation.java
    	modified:   idas.chox.web/src/main/webapp/scripts/general.js

--- idas.chox.web/src/main/webapp/scripts/general.js
+++ idas.chox.web/src/main/webapp/scripts/general.js
@@ -104,7 +104,7 @@ function onOpenAbout(){
     var msg = "<span class='aboutProductName'>Product Name: iDAS CHOX</span><br/><br/>";
 
     msg = msg + "<span class='acountCopyright'>Copyright Message: &copy;2010 Sherwood Compliance Services Ltd</span><br/><br/>";
-    msg = msg + "<span class='acountVersionNumber'>V3.5 - 20100628</span><br/><br/>";
+    msg = msg + "<span class='acountVersionNumber'>V3.5 - 20100701</span><br/><br/>";
     msg = msg + "<input type='button' value='Close' onclick='javascript:$.unblockUI();'>";
 
     $.blockUI({
