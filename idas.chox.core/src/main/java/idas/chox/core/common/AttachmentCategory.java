5b39867c50938110a49936dc7c6fc77ab9554547
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2010-07-26 13:29:42 +0100

    Implemented to-do item (Phase 4 Sprint 2) 'Additional drop down options on 'Attachments' tab'.
    	modified:   idas.chox.core/src/main/java/idas/chox/core/common/AttachmentCategory.java

--- idas.chox.core/src/main/java/idas/chox/core/common/AttachmentCategory.java
+++ idas.chox.core/src/main/java/idas/chox/core/common/AttachmentCategory.java
@@ -7,12 +7,26 @@ public class AttachmentCategory {
     
     public static final String ATTCAT_PAYMENTPACK = "Payment Pack";
     public static final String ATTCAT_TOTALLOSS = "Total Loss Inspection Check";
+    public static final String ATTCAT_CHO_ALLEGATIONS = "CHO's Client Allegations";
+    public static final String ATTCAT_INS_ALLEGATIONS = "Insurer's Client Allegations";
+    public static final String ATTCAT_INV_REPORTS = "Investigator Reports";
+    public static final String ATTCAT_REPAIR_DOCS = "Repair Documents";
+    public static final String ATTCAT_REPAIR_STATEMENT = "Repairer Statement";
+    public static final String ATTCAT_TOTALLOSS_PACK = "Total Loss Pack";
+    public static final String ATTCAT_WITNESS_STATEMENT = "Witness Statement";
     public static final String ATTCAT_OTHER = "Other";
-    
+
     public static List<String> getAttachmentCategory() {
         List<String> status = new ArrayList<String>();
         status.add(ATTCAT_PAYMENTPACK);
         status.add(ATTCAT_TOTALLOSS);
+        status.add(ATTCAT_CHO_ALLEGATIONS);
+        status.add(ATTCAT_INS_ALLEGATIONS);
+        status.add(ATTCAT_INV_REPORTS);
+        status.add(ATTCAT_REPAIR_DOCS);
+        status.add(ATTCAT_REPAIR_STATEMENT);
+        status.add(ATTCAT_TOTALLOSS_PACK);
+        status.add(ATTCAT_WITNESS_STATEMENT);
         status.add(ATTCAT_OTHER);
         return status;
     }
