78f22f7c04b445a82e21540a8272d1d70f262f55
seenimurugan shanmugam <seeni@ip100.sherwood2.wf-net.co.uk>
2010-11-25 18:41:10 +0000

    InvoiceRecalculation - Custom error message removed from Base Action
    	modified:   idas.chox.web/src/main/java/idas/chox/web/actions/BaseAction.java

--- idas.chox.web/src/main/java/idas/chox/web/actions/BaseAction.java
+++ idas.chox.web/src/main/java/idas/chox/web/actions/BaseAction.java
@@ -274,6 +274,6 @@ public class BaseAction extends ActionSupport {
             return "Record was updated by another transaction/user, please try again.";
         }
         LOG.debug("handleException: exception is {} and changing that exception to user friendly exception ('Could not Update the Value : Please Check the Modified Value and Resubmit If you still see the same Message Please contact the Chox Admin Support')", ex.getMessage());
-        return "Could not Update the Value : Please Check the Modified Value and Resubmit, If you still see the same Error Message Please contact the Chox Admin Support";
+        return ex.getMessage();
     }
 }
