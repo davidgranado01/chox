ae1a9748128d0a07cd0113ab62e5ab7683e45449
rajareddy dodda <rajareddydodda@ip83.sherwood2.wf-net.co.uk>
2010-11-29 14:45:20 +0000

    TODO-Mandatory supporting notes when rejecting an invoice-CodeCleanup and added code to clean error messages

--- idas.chox.core/src/main/java/idas/chox/core/model/Invoice.java
+++ idas.chox.core/src/main/java/idas/chox/core/model/Invoice.java
@@ -193,7 +193,7 @@ public class Invoice extends Entity implements Serializable {
     protected BigDecimal interimPayment;
     protected Boolean interimPaymentReceived;
     protected String interimPaymentReceivedDesc;
-     private String supportingRejectionNotes;
+    
 
 
 
@@ -1238,18 +1238,6 @@ public class Invoice extends Entity implements Serializable {
         this.totalPenaltyCharge = totalPenaltyCharge;
     }
 
-    /**
-     * @return the supportingRejectionNotes
-     */
-    public String getSupportingRejectionNotes() {
-        return supportingRejectionNotes;
-    }
-
-    /**
-     * @param supportingRejectionNotes the supportingRejectionNotes to set
-     */
-    public void setSupportingRejectionNotes(String supportingRejectionNotes) {
-        this.supportingRejectionNotes = supportingRejectionNotes;
-    }
+    
 
 }
