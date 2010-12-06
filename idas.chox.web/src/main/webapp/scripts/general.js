c112a491137f25733a0f391cc09fc3e581a7402f
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2010-12-06 09:54:59 +0000

    Reset to operational values.
    	modified:   idas.chox.web/src/main/resources/applicationContext.xml
    	modified:   idas.chox.web/src/main/resources/log4j.properties
    	modified:   idas.chox.web/src/main/webapp/WEB-INF/web.xml
    	modified:   idas.chox.web/src/main/webapp/jsp/Exception.jsp
    	modified:   idas.chox.web/src/main/webapp/scripts/general.js

--- idas.chox.web/src/main/webapp/scripts/general.js
+++ idas.chox.web/src/main/webapp/scripts/general.js
@@ -110,7 +110,7 @@ function onOpenAbout(){
     var msg = "<span class='aboutProductName'>Product Name: CHOX</span><br/><br/>";
 
     msg = msg + "<span class='acountCopyright'>Copyright Message: &copy;2010 Sherwood Compliance Services Ltd</span><br/><br/>";
-    msg = msg + "<span class='acountVersionNumber'>V3.11 - 20101203</span><br/><br/>";
+    msg = msg + "<span class='acountVersionNumber'>V3.11 - 20101206</span><br/><br/>";
     msg = msg + "<input type='button' value='Close' onclick='javascript:$.unblockUI();'>";
 
     $.blockUI({
