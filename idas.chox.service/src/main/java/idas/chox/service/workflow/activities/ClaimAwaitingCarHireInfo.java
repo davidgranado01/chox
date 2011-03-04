8852a679dcbe8ae10a8917e479ece5b140f0d300
seenimurugan shanmugam <seeni@ip59.sherwood2.wf-net.co.uk>
2011-03-04 15:30:50 +0000

    Hire monitering Validation Error shows empty box fixed

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ClaimAwaitingCarHireInfo.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ClaimAwaitingCarHireInfo.java
@@ -5,9 +5,12 @@ import idas.chox.core.model.ClaimStatus;
 import idas.chox.core.security.SecurityInfoProvider;
 import java.util.List;
 import org.springframework.security.AccessDeniedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClaimAwaitingCarHireInfo extends BaseActivity {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ClaimAwaitingCarHireInfo.class);
     private StringBuffer sb = new StringBuffer(550);
     private StringBuffer sb1 = new StringBuffer(150);
 
@@ -20,6 +23,7 @@ public class ClaimAwaitingCarHireInfo extends BaseActivity {
             if (claim.getHireMonitoringEcds().isEmpty()) {
                 //sb.append("* You need to provide an Estimated Completion Date (ECD) to submit this claim.");
                 sb1.append("* You need to provide an Estimated Completion Date (ECD) to submit this claim.");
+
             }
         }
 
@@ -27,19 +31,26 @@ public class ClaimAwaitingCarHireInfo extends BaseActivity {
             //sb.append("* In order to progress the claim, entries in either 'Labour Hours' or 'Total Labour Cost' fields are required,.if this information cannot be provided please select the reason why using the 'Labour Information Non-Provision Reason' drop down box.");
             sb1.append("* In order to progress the claim, entries in either 'Labour Hours' or 'Total Labour Cost' fields are required,.if this information cannot be provided please select the reason why using the 'Labour Information Non-Provision Reason' drop down box.");
         }
-        if (!claim.getHireMonitoringDetail().isIsRepairOnlyCheck()) {
+        if (claim.getHireMonitoringDetail() != null && !claim.getHireMonitoringDetail().isIsRepairOnlyCheck()) {
             if (claim.getHireMonitoringDetail().isIsTotalLostCheck()) {
                 if (!isRequiredFieldPresentWhenTotalLossChecked(claim)) {
                     throw new Exception(sb.toString());
                 }
             } else {
-                if(!isRequiredFieldPresentWhenTotalLossUnChecked(claim)){
+                if (!isRequiredFieldPresentWhenTotalLossUnChecked(claim)) {
+                    LOG.debug("throwing validation exception error {} for claim {}", sb1, claim.getChoReference());
                     throw new Exception(sb.toString());
                 }
             }
+        } else if (claim.getHireMonitoringDetail() == null) {
+            sb1.append("* For Hires That Involved A Repair The Following Fields Are Required.");
+            sb1.append("'Date Repair Authorised'.");
+            sb1.append("'Repair Completion Date'.");
+            throw new Exception(sb1.toString());
         }
         //if(!sb1.toString().equals("")||sb1!=null){
-        if(sb1.length()>0){
+        if (sb1.length() > 0) {
+            LOG.debug("throwing validation exception error {} for claim {}", sb1, claim.getChoReference());
             throw new Exception(sb1.toString());
         }
         SecurityInfoProvider securityInfoProvider = this.getWorkflowContext().getSecurityInfoProvider();
@@ -109,8 +120,8 @@ public class ClaimAwaitingCarHireInfo extends BaseActivity {
 
     public boolean isRequiredFieldPresentWhenTotalLossChecked(Claim claim) {
         boolean result = true;
-           sb.append(sb1.toString());
-           sb.append("* For Hires That Involved A Total Loss The Following Fields Are Required.");
+        sb.append(sb1.toString());
+        sb.append("* For Hires That Involved A Total Loss The Following Fields Are Required.");
         if (claim.getHireMonitoringDetail().getTotalLossOfferMadeDate() == null) {
             sb.append("'Date Total Loss Offer Made'.");
             result = false;
@@ -124,7 +135,7 @@ public class ClaimAwaitingCarHireInfo extends BaseActivity {
 //            result = false;
 //        }
 //        if (claim.getHireMonitoringDetail().getTotalLossOfferCheckReceivedDate() == null) {
-//            sb.append("'Repair Book In Dates'.");
+//            sb.append("'Datee Check Received'.");
 //            result = false;
 //        }
 
