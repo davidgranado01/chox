8365781b299206f71f24c243f9f51f7b1a2e4aa0
seenimurugan shanmugam <seeni@ip59.sherwood2.wf-net.co.uk>
2011-05-05 17:01:45 +0100

    admin testing is 100 percent

--- idas.chox.service/src/test/java/idas/chox/service/xml/BaseXMLUploadClaimTest.java
+++ idas.chox.service/src/test/java/idas/chox/service/xml/BaseXMLUploadClaimTest.java
@@ -1,25 +1,25 @@
-package idas.chox.service.xml;
-
-import idas.chox.core.services.BordereauService;
-import idas.chox.core.services.ClaimService;
-import idas.chox.core.services.UploadClaimXMLService;
-import idas.chox.core.services.VehicleClassService;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext-test.xml", "classpath:applicationContext-services-test.xml", "classpath:applicationContext-XMLReader-test.xml", "classpath:applicationContext-Workflow-test.xml", "classpath:applicationContext-services-test.xml", "classpath:applicationContext-BRE-test.xml"})
-public abstract class BaseXMLUploadClaimTest {
-
-    @Autowired
-    protected UploadClaimXMLService uploadClaimXMLService;
-    @Autowired
-    protected BordereauService bordereauService;
-    @Autowired
-    protected ClaimService claimService;
-    @Autowired
-    protected VehicleClassService vehicleClassService;
-
-}
+//package idas.chox.service.xml;
+//
+//import idas.chox.core.services.BordereauService;
+//import idas.chox.core.services.ClaimService;
+//import idas.chox.core.services.UploadClaimXMLService;
+//import idas.chox.core.services.VehicleClassService;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:applicationContext-test.xml", "classpath:applicationContext-services-test.xml", "classpath:applicationContext-XMLReader-test.xml", "classpath:applicationContext-Workflow-test.xml", "classpath:applicationContext-services-test.xml", "classpath:applicationContext-BRE-test.xml"})
+//public abstract class BaseXMLUploadClaimTest {
+//
+//    @Autowired
+//    protected UploadClaimXMLService uploadClaimXMLService;
+//    @Autowired
+//    protected BordereauService bordereauService;
+//    @Autowired
+//    protected ClaimService claimService;
+//    @Autowired
+//    protected VehicleClassService vehicleClassService;
+//
+//}
