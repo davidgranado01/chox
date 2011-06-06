df439e5c84f3274d7d6b65e970acfd038e906a06
seenimurugan shanmugam <seeni@ip59.sherwood2.wf-net.co.uk>
2011-06-06 13:08:27 +0100

    BUG#964 concurrency inbox queue error fixed

---
+++ idas.chox.web/src/main/java/idas/chox/web/viewdata/FilterViewData.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idas.chox.web.viewdata;
+
+/**
+ *
+ * @author seeni
+ */
+public class FilterViewData {
+
+    private String key;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
