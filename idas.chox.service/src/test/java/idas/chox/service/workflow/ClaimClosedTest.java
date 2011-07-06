cab2cabd2a8a44c0b7bf5ea4b14beabaacdca47e
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2011-07-06 16:35:32 +0100

    Tests updated.
    	modified:   idas.chox.service/src/test/java/idas/chox/service/workflow/ClaimClosedTest.java
    	modified:   idas.chox.service/src/test/java/idas/chox/service/workflow/ClaimRejectionAcceptTest.java
    	modified:   idas.chox.service/src/test/java/idas/chox/service/workflow/ClaimReopenTest.java
    	modified:   idas.chox.service/src/test/java/idas/chox/service/workflow/ClaimRevertTest.java
    	modified:   idas.chox.service/src/test/java/idas/chox/service/workflow/InvoicePaymentReceivedTest.java
    	modified:   idas.chox.service/src/test/java/idas/chox/service/workflow/InvoiceRejectionAcceptTest.java
    	modified:   idas.chox.service/src/test/resources/applicationContext-Workflow-test.xml

--- idas.chox.service/src/test/java/idas/chox/service/workflow/ClaimClosedTest.java
+++ idas.chox.service/src/test/java/idas/chox/service/workflow/ClaimClosedTest.java
@@ -1,38 +1,34 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package idas.chox.service.workflow;
 
 import idas.chox.core.model.Claim;
 import idas.chox.core.model.ClaimStatus;
-import idas.chox.core.model.Insurer;
-import idas.chox.core.model.ThirdParty;
-import idas.chox.core.services.InsurerService;
-import idas.chox.core.workflow.Activity;
-import idas.chox.core.workflow.exceptions.InvalidClaimStatusException;
-import idas.chox.service.workflow.activities.AssignOwner;
-import idas.chox.service.workflow.activities.AutoRouting;
+import idas.chox.core.services.ClaimService;
 import idas.chox.service.workflow.activities.ClaimClosed;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext-workflow-test.xml", "classpath:applicationContext-test.xml", "classpath:applicationContext-services-test.xml", "classpath:applicationContext-BRE-test.xml"})
+@ContextConfiguration(locations = {"classpath:applicationContext-Workflow-test.xml", "classpath:applicationContext-test.xml", "classpath:applicationContext-services-test.xml", "classpath:applicationContext-BRE-test.xml"})
 public class ClaimClosedTest {
 
     @Autowired
     ActivityFactory activityFactory;
 
-   
+    @Autowired
+    ClaimService claimService;
+
     @Test
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void testClaimClosed() throws Throwable {
 
         Claim claim = new Claim();
         claim.setStatus(ClaimStatus.CLAIM_UNACKNOWLEDGED_UNROUTED);
+        claimService.saveClaimWithoutUpdatingLiabilityPayment(claim);
         ClaimClosed activity = (ClaimClosed) activityFactory.getActivity("closeClaim");
         activity.process(claim);
         Assert.assertEquals(ClaimStatus.CLAIM_CLOSED, claim.getStatus());
