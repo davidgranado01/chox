dff32407f8b5720e25ef40417d35fa7ed9d78901
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2011-07-16 09:24:52 +0100

    Build number updated for release.
    	modified:   idas.chox.web/src/main/webapp/scripts/general.js

--- idas.chox.web/src/main/webapp/scripts/general.js
+++ idas.chox.web/src/main/webapp/scripts/general.js
@@ -110,7 +110,7 @@ function onOpenAbout(){
     var msg = "<span class='aboutProductName'>Product Name: CHOX</span><br/><br/>";
 
     msg = msg + "<span class='acountCopyright'>Copyright Message: &copy;2010 Sherwood Compliance Services Ltd</span><br/><br/>";
-    msg = msg + "<span class='acountVersionNumber'>V4.7 - 20110715</span><br/><br/>";
+    msg = msg + "<span class='acountVersionNumber'>V4.7 - 20110716</span><br/><br/>";
     msg = msg + "<input type='button' value='Close' onclick='javascript:$.unblockUI();'>";
 
     $.blockUI({
