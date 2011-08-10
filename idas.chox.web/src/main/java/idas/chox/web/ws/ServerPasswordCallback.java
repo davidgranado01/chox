45d57bbdeddef43c6e2d4cafda66a6040e61b10a
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2011-08-10 18:21:10 +0100

    Password validation working (PasswordText only, not PasswordDigest).
    	modified:   idas.chox.web/pom.xml
    	modified:   idas.chox.web/src/main/java/idas/chox/web/ws/ServerPasswordCallback.java
    	new file:   idas.chox.web/src/main/java/idas/chox/web/ws/WSAuthenticationInInterceptor.java
    	new file:   idas.chox.web/src/main/java/idas/chox/web/ws/WSTokenValidator.java
    	modified:   idas.chox.web/src/main/resources/applicationContext-WebServices.xml
    	modified:   uploadClient/src/client.xml

--- idas.chox.web/src/main/java/idas/chox/web/ws/ServerPasswordCallback.java
+++ idas.chox.web/src/main/java/idas/chox/web/ws/ServerPasswordCallback.java
@@ -2,6 +2,8 @@ package idas.chox.web.ws;
 
 import idas.chox.web.security.WebUserService;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
@@ -9,21 +11,26 @@ import org.apache.ws.security.WSPasswordCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.userdetails.UserDetails;
-import org.springframework.security.userdetails.UserDetailsService;
 
 /**
  *
  * @author John
  */
 public class ServerPasswordCallback implements CallbackHandler {
-    static final Logger LOG = LoggerFactory.getLogger(ServerPasswordCallback.class);
 
-//    private UserDetailsService service;
+    static final Logger LOG = LoggerFactory.getLogger(ServerPasswordCallback.class);
     private WebUserService userDetailsService;
-    
+
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-        WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+        WSPasswordCallback pc = null;
+        for (Callback callback : callbacks) {
+            if (callback instanceof WSPasswordCallback) {
+                pc = (WSPasswordCallback) callback;
+                break;
+            }
+        }
+//        WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
         LOG.debug("PasswordCallbackHandler: identifier is '{}'", pc.getIdentifier());
         LOG.debug("PasswordCallbackHandler: password is '{}'", pc.getPassword());
 
@@ -33,17 +40,18 @@ public class ServerPasswordCallback implements CallbackHandler {
         // this seems ridiculous, but is necessary for passing authentication on
         // to Spring-Security. We're essentially bypassing CXF's WSS4JInterceptor
         // by ensuring that the password callback always matches the client password.
-//        pc.setPassword(pc.getPassword());
+        String pass = userDetails.getPassword();
+        if (pass != null) {
+            LOG.debug("Password set: '{}'", pass);
+            pc.setPassword(pass);
+            return;
+        }
+
         pc.setPassword(userDetails.getPassword());
 
     }
 
-//    public void setService(UserDetailsService service) {
-//        this.service = service;
-//    }
-
     public void setUserDetailsService(WebUserService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
-
 }
