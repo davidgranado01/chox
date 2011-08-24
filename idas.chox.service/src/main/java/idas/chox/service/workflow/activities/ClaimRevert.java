2250e01fe23563991e4171008c8ff8dcfcb8ba90
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2011-08-24 15:00:53 +0100

    Bug#1111 fixed: Production - Allow Insurer Manager To Use Revert Status.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ClaimRevert.java
    
    	modified:   Configuration/DBScript/Phase5Sprint10.txt

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ClaimRevert.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ClaimRevert.java
@@ -31,6 +31,7 @@ public class ClaimRevert extends BaseActivity {
                 && !claim.getStatus().equals(ClaimStatus.CLAIM_REJECTION_ACCEPTED) && !claim.getStatus().equals(ClaimStatus.INVOICE_REJECTED_ACCEPTED)
                 && !claim.getStatus().equals(ClaimStatus.INVOICE_PAYMENT_LOGGED))
                 || (securityInfoProvider.getIsINS() && !claim.getStatus().equals(ClaimStatus.CLAIM_REFERRED_TO_FNOL)
+                && !claim.getStatus().equals(ClaimStatus.AWAITING_INVOICE_PAYMENT) && !claim.getStatus().equals(ClaimStatus.AWAITING_LIABILITY_RESOLUTION)
                 && !claim.getStatus().equals(ClaimStatus.CLAIM_REF_TO_ENG) && !claim.getStatus().equals(ClaimStatus.INVOICE_REF_TO_ENG)
                 && !claim.getStatus().equals(ClaimStatus.INVOICE_PAYMENT_LOGGED) && !claim.getStatus().equals(ClaimStatus.CLAIM_AWAITING_INVOICE_DATA))) {
             throw new AccessDeniedException("Not in correct role to revert claim in status '" + claim.getStatus() + "'.");
