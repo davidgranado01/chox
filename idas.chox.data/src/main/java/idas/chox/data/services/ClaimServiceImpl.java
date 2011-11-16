cd52478f72bc320e4d14714de92b5dcc90b1142a
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2011-11-16 16:38:03 +0000

    Added filter for claims where penalty charges not allowed (for queue).
    	modified:   idas.chox.data/src/main/java/idas/chox/data/services/ClaimServiceImpl.java

--- idas.chox.data/src/main/java/idas/chox/data/services/ClaimServiceImpl.java
+++ idas.chox.data/src/main/java/idas/chox/data/services/ClaimServiceImpl.java
@@ -1,5 +1,6 @@
 package idas.chox.data.services;
 
+import idas.chox.core.common.OrganisationType;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,6 +22,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import idas.chox.core.model.AuditTrail;
+import idas.chox.core.model.BreBand;
+import idas.chox.core.model.BreBandOrganisation;
 import idas.chox.core.model.Claim;
 import idas.chox.core.model.ClaimStatus;
 import idas.chox.core.model.Comment;
@@ -37,6 +40,7 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.Calendar;
 import org.apache.http.impl.cookie.DateUtils;
+import org.hibernate.criterion.Property;
 
 public class ClaimServiceImpl extends SecureDataService implements ClaimService, Serializable {
 
@@ -617,9 +621,6 @@ public class ClaimServiceImpl extends SecureDataService implements ClaimService,
             anomaliesStatus.add(ClaimStatus.CLAIM_UPDATE_BY_ENG);
             anomaliesStatus.add(ClaimStatus.CLAIM_UNACKNOWLEDGED_ROUTED);
 
-
-
-
             DetachedCriteria noti = DetachedCriteria.forClass(Notification.class).add(Restrictions.in("type", NotificationType.getInsurerNotificationTypes())).add(Restrictions.eq("isacknowledged", false)).setProjection(Projections.projectionList().add(Projections.property("claim")));
             criteria.add(Subqueries.propertyIn("id", noti));
             criteria.add(Restrictions.in("status", anomaliesStatus));
@@ -639,6 +640,31 @@ public class ClaimServiceImpl extends SecureDataService implements ClaimService,
             criteria.add(Restrictions.ne("status", ClaimStatus.INVOICE_DATA_CALCULATION_INCORRECT));
             criteria.add(Restrictions.ge("iv.penaltyAlertQty", 0));
             criteria.add(Restrictions.sqlRestriction("extract(epoch from current_date- iv1_.created_date)/(3600*24) >(iv1_.penalty_alert_qty+1)*30"));
+
+            if (!OrganisationType.CHO.equals(getCurrentUser().getOrganisationType())) {
+                LOG.warn("Error in search criteria: only CHO can filter for penalty charges");
+            }
+            else {
+                LOG.debug("Supplier Id={}", getCurrentUser().getChorganisation().getId());
+                DetachedCriteria bCriteria =  DetachedCriteria.forClass(BreBandOrganisation.class, "brebandorganisation")
+                    .createAlias("brebandorganisation.chorganisation", "cho", CriteriaSpecification.LEFT_JOIN)
+                    .add(Restrictions.eq("cho.id", getCurrentUser().getChorganisation().getId()));
+                bCriteria.setProjection( Projections.property("brebandorganisation.breBand.id") );
+
+/**
+                List<Integer> bList = bCriteria.getExecutableCriteria(getSession()).list();
+                
+                for(Integer b : bList) {
+                    LOG.info("Found BreBand with id={} [BreBand mapped to my CHO with id={}]", b, getCurrentUser().getChorganisation().getId());
+                }
+**/
+                DetachedCriteria pCriteria =  DetachedCriteria.forClass(BreBand.class, "breband")
+                    .add(Restrictions.eq("breband.allowPenaltyCharges", Boolean.FALSE))
+                    .add(Restrictions.in("breband.id", bCriteria.getExecutableCriteria(getSession()).list() ))
+                    .setProjection( Projections.property("breband.insurer") );
+
+                criteria.add(Property.forName("this.insurer").notIn( pCriteria ) );
+            }
 
             Junction nonSplit = Restrictions.disjunction().add(Restrictions.isNull("liabilityStatus"))
                     .add(Restrictions.conjunction().add(Restrictions.ne("liabilityStatus", LiabilityStatus.LIABILITY_SPLIT))
