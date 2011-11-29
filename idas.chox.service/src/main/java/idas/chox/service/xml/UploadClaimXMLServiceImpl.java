e2250d095266386b69aa056244db3a52372af2c3
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2011-11-29 10:25:47 +0000

    Minor changes to log messages.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/xml/UploadClaimXMLServiceImpl.java
    	modified:   idas.chox.web/src/main/java/idas/chox/web/actions/ExcelGeneratorAction.java
    	modified:   idas.chox.web/src/main/java/idas/chox/web/actions/ReportAction.java

--- idas.chox.service/src/main/java/idas/chox/service/xml/UploadClaimXMLServiceImpl.java
+++ idas.chox.service/src/main/java/idas/chox/service/xml/UploadClaimXMLServiceImpl.java
@@ -360,12 +360,12 @@ public class UploadClaimXMLServiceImpl extends SecureDataService implements Uplo
                     }
                 }
                 claimsDetails.add(0, xmlClaimsDetail);
-                LOG.info("Synchronizing on session");
+                LOG.debug("Synchronizing on session");
                 synchronized (session) {
                     session.put("claimsDetails", claimsDetails);
                 }
-                LOG.info("Finished synchronizing on session");
-                LOG.debug("putting claimDetails into session total size is: {}", claimsDetails.size());
+                LOG.debug("Finished synchronizing on session");
+                LOG.debug("claimDetails added to session - total size of claimDetails is: {}", claimsDetails.size());
                 LOG.debug("{} of {} claims have been processed", totalRecord, totalProcessed);
             }
         } catch (Throwable ex) {
