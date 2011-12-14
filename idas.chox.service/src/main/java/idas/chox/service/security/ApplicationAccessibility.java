ef7c08317671bdb29565433b7ff109bf2a5ff77a
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2011-12-14 18:48:00 +0000

    Fixes for updates from Sprint 5.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/security/ApplicationAccessibility.java
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ClaimReopen.java

--- idas.chox.service/src/main/java/idas/chox/service/security/ApplicationAccessibility.java
+++ idas.chox.service/src/main/java/idas/chox/service/security/ApplicationAccessibility.java
@@ -471,7 +471,7 @@ public class ApplicationAccessibility {
             HashMap roleMap = (HashMap) getAccessibilityMap().get(accessibilityKey);
             Short accessRight = checkAccessibility(roleMap, user);
             
-            if (user.isAnInsurer() && buttonName.equalsIgnoreCase(ApplicationAccessibility.REOPEN_CLAIM) && !claim.isInsurerUpload()) {
+            if (user.isAnInsurer() && buttonName.equalsIgnoreCase(ApplicationAccessibility.REOPEN_CLAIM) && !ClaimType.isInsurerUpload(claim.getClaimType())) {
                 LOG.debug("Declined access to Button accessibility (ReOpen claim) as this claim is not insurer uploaded.");
                 return Declined;
             }
