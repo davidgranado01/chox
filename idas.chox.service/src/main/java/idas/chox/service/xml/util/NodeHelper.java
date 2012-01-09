432eed56ddc1778b87416b29fd548f29a54577cb
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2012-01-09 14:07:49 +0000

    Fixed bug#1418 - Production - Error message for wrong Insurer on claim upload needs changing.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/xml/util/NodeHelper.java

--- idas.chox.service/src/main/java/idas/chox/service/xml/util/NodeHelper.java
+++ idas.chox.service/src/main/java/idas/chox/service/xml/util/NodeHelper.java
@@ -27,7 +27,7 @@ public class NodeHelper {
     private static String INCORRECT_DATA_LENGTH_ERROR_MSG = "Length for '%s' field is bigger than allowed limit of '%s' characters. Please amend and re-submit.";
     private static String IncorrectDataErrorMsg = "Invalid or incorrect character in '%s' for '%s'.";
     private static String mandatoryVehicleClassDataErrorMsg = "Selected Vehicle Class is invalid for '%s'";
-    private static String IncorrectInsurerAlias = "Selected '%s' for 'Supplier Name' is invalid; this alias does not exist";
+    private static String IncorrectInsurerAlias = "Selected '%s' for 'Third Party Insurer' is invalid; this alias does not exist";
     private static String IncorrectChorganisationAlias = "Selected '%s' for 'Supplier Name' is invalid, this alias does not exist";
     public static final String REG_TIMESTAMP = "^\\d{4}-(0[0-9]|1[0,1,2])-([0-9]|[0,1,2][0-9]|3[0,1])[T]([0-9]{2}):([0-9]{2}):([0-9]{2})$";
 //    public static final String REG_DATETIME = "^([0-9]|[0,1,2][0-9]|3[0,1])/(0[0-9]|1[0,1,2])/\\d{4}.*$";
