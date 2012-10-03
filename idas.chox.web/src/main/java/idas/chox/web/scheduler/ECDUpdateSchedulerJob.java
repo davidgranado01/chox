284dec70317b62a2a13a82735b2281d80e96c3b4
patrikbego <patrik.bego@sherwoodcompliance.co.uk>
2012-10-03 17:29:11 +0100

    2154 - Added new line character on the end of the line.

--- idas.chox.web/src/main/java/idas/chox/web/scheduler/ECDUpdateSchedulerJob.java
+++ idas.chox.web/src/main/java/idas/chox/web/scheduler/ECDUpdateSchedulerJob.java
@@ -118,7 +118,7 @@ public class ECDUpdateSchedulerJob extends EmailSchedulerJob {
             }
   
         } else {
-            emailMsg.append("No attachement on email, please check and re-submit.");
+            emailMsg.append("No attachement on email, please check and re-submit.\n");
             emailMsg.append("-----------------------------------------------------------------------------------------------\n");
         }
         LOG.debug("Message to send is: \n*********\n{}\n*********", emailMsg.toString());
