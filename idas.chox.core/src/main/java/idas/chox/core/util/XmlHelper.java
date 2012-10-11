9330c00bd7316c1ba8e977a3a29c0b2e67a9ddbe
seenimurugan <seeni.shanmugam@sherwoodcompliance.co.uk>
2012-10-11 09:26:02 +0100

    Bug#2176 - Production - More than 2 decimal places are allowed on cost fields when uploading invoice

--- idas.chox.core/src/main/java/idas/chox/core/util/XmlHelper.java
+++ idas.chox.core/src/main/java/idas/chox/core/util/XmlHelper.java
@@ -63,11 +63,11 @@ public class XmlHelper {
 
     public static BigDecimal getBigDecimalFromNode(Element thisElement, String thisNodeName) {
 
-        BigDecimal bOutput = new BigDecimal("0.00");
+        BigDecimal bOutput = BigDecimal.ZERO.setScale(2);
         String sOutput = XMLUtils.getElementValue(thisElement, thisNodeName);
 
         if (sOutput != null && !sOutput.equalsIgnoreCase("") && sOutput.length() > 0) {
-            bOutput = new BigDecimal(sOutput);
+            bOutput = new BigDecimal(sOutput).setScale(2, BigDecimal.ROUND_HALF_UP);
         }
 
         return bOutput;
