ea39e74698af2999d923d44b4473f7898b69fe39
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2012-11-13 17:40:22 +0000

    Tests updated.
    	modified:   idas.chox.service/src/test/java/idas/chox/service/workflow/SubscriberClaimRejectionAcceptTest.java
    
    Change-Id: I5027002e1b7f4e269943f013ed3e4b1c0c3624bb

--- idas.chox.service/src/test/java/idas/chox/service/workflow/SubscriberClaimRejectionAcceptTest.java
+++ idas.chox.service/src/test/java/idas/chox/service/workflow/SubscriberClaimRejectionAcceptTest.java
@@ -43,7 +43,7 @@ public class SubscriberClaimRejectionAcceptTest extends BaseTest {
 
 
         activity.process(claim);
-        Assert.assertEquals(ClaimStatus.CLAIM_AWAITING_INVOICE_DATA, claim.getStatus());
+        Assert.assertEquals(ClaimStatus.CLAIM_AWAITING_CAR_HIRE_INFO, claim.getStatus());
     }
 
 }
