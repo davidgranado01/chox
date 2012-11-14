e20b982bbe370cf76cff9fe7d2326e94a41fe20b
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2012-11-14 14:33:47 +0000

    Update to move directly to AwaitingInvoiceData when rejection accepted, as ir previously worked. Update due to comment by BR.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/SubscriberClaimRejectionAccept.java
    
    Change-Id: Ifa10958c0eec5f860d0cacf9983a0a8a845e4e94

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/SubscriberClaimRejectionAccept.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/SubscriberClaimRejectionAccept.java
@@ -40,9 +40,10 @@ public class SubscriberClaimRejectionAccept extends BaseActivity {
         if (reasonOfRejectionService.isSubscriberClaimRejected(claim.getReasonOfRejection())) {
              claim.setStatus(ClaimStatus.CLAIM_REJECTION_ACCEPTED);
        } else {
-//            setCurrentStatus(ClaimStatus.CLAIM_AWAITING_CAR_HIRE_INFO);
+            setCurrentStatus(ClaimStatus.CLAIM_AWAITING_CAR_HIRE_INFO);
             claim.setStatus(ClaimStatus.CLAIM_AWAITING_CAR_HIRE_INFO);
-//            logTransaction(claim, ClaimStatus.SUBSCRIBER_CLAIM_REJECTED, claim.getReasonOfRejection(), null);            
+            logTransaction(claim, ClaimStatus.SUBSCRIBER_CLAIM_REJECTED, claim.getReasonOfRejection(), null);
+            claim.setStatus(ClaimStatus.CLAIM_AWAITING_INVOICE_DATA);
         }
     }
 
