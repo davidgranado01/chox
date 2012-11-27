9690f7e76563adae156d0d6e54bafc228fc3174a
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2012-11-27 16:14:33 +0000

    Implemented new requirement on todo item 7.1.7 - Fixed Fee Claims: For 'Fixed Fee' claims there are 2 liability statuses that can be selected at the front end when acknowledging a claim, 'Full Liability Accepted' as above and also 'Liability Split', the validation for the 'Liability Split' that is currently in place for GTA claims should remain as is.
    	modified:   idas.chox.data/src/main/java/idas/chox/data/services/ClaimServiceImpl.java
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/InvoicePaymentLogged.java
    	modified:   idas.chox.web/src/main/java/idas/chox/web/actions/ClaimAction.java
    	modified:   idas.chox.web/src/main/webapp/jsp/actionPanel/s_liability_validation.jspf
    
    Change-Id: Ic241e945faf253730c844ea711cb6d2fb2be7f70

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/InvoicePaymentLogged.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/InvoicePaymentLogged.java
@@ -97,8 +97,7 @@ public class InvoicePaymentLogged extends BaseActivity {
     protected void doProcess(Claim claim) {
         Invoice invoice = claim.getInvoice();
         if (finalPayment == null) { // Ok hit - no fields changed. Take values from invoice
-            if (ClaimType.isInsurerVsInsurer(claim.getClaimType()) || ClaimType.isSubscriber(claim.getClaimType())
-                    || ClaimType.isFixedFee(claim.getClaimType()) ) {
+            if (ClaimType.isInsurerVsInsurer(claim.getClaimType()) || ClaimType.isSubscriber(claim.getClaimType()) ) {
                 invoice.setHireGrossPaid(invoice.getHireGross());
                 invoice.setRepairGrossPaid(invoice.getRepairGross());
                 invoice.setEngineerFeeGrossPaid(invoice.getEngineerFeeGross());
