00f70d7de044af0c8b0ec8b175ce563b01e1f88d
patrikbego <patrik.bego@sherwoodcompliance.co.uk>
2012-12-19 10:56:41 +0000

    Added additional unit tests.

--- idas.chox.service/src/test/java/idas/chox/service/workflow/SubscriberClaimToGtaTest.java
+++ idas.chox.service/src/test/java/idas/chox/service/workflow/SubscriberClaimToGtaTest.java
@@ -28,79 +28,81 @@ public class SubscriberClaimToGtaTest extends BaseTest {
         Activity activity = activityFactory.getActivity("sendClaimGTA");
         activity.process(claim);
     }
+    
+    @Test(expected = InvalidClaimStatusException.class)
+    public void testAttemptToSwichToGtaStatus() throws Exception {
+
+        Claim claim = new Claim();
+        claim.setClaimType(ClaimType.GTA);
+        claim.setStatus(ClaimStatus.CLAIM_UNACKNOWLEDGED_UNROUTED);
+        claimService.saveClaimWithoutUpdatingLiabilityPayment(claim);
+        Activity activity = activityFactory.getActivity("sendClaimGTA");
+        activity.process(claim);
+    }
 
     @Test
-    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void testSubscriberClaimRejectionContest() throws Throwable {
-        Claim claim = claimService.getClaim(999);
+        Claim claim = new Claim();
+        claim.setId(997);
         claim.setClaimType(ClaimType.SUBSCRIBER);
         Insurer insurer = insurerService.getInsurer(3);
         claim.setInsurer(insurer);
         claim.setStatus(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED);
-        claimService.saveClaimWithoutUpdatingLiabilityPayment(claim);
         Activity activity = activityFactory.getActivity("sendClaimGTA");
 
         activity.process(claim);
-        // TODO: here we should check that the status has been set to its "previous" status
-        //        - need to set up audit trail. For now, we'll check that the status has changed...
-        Assert.assertNotSame(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED, claim.getStatus());
+        Assert.assertEquals(ClaimStatus.CLAIM_UNACKNOWLEDGED_ROUTED, claim.getStatus());
         Assert.assertEquals(claim.getClaimType(), ClaimType.GTA);
         Assert.assertTrue(claim.getComments().get(0).getComment().contains("Subscriber"));
     }
     
     @Test
-    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void testFixedFeeClaimRejectionContest() throws Throwable {
         Claim claim = new Claim();
+        claim.setId(996);
         claim.setClaimType(ClaimType.FIXED_FEE);
         Insurer insurer = insurerService.getInsurer(3);
         claim.setInsurer(insurer);
         claim.setStatus(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED);
-        claimService.saveClaimWithoutUpdatingLiabilityPayment(claim);
         Activity activity = activityFactory.getActivity("sendClaimGTA");
 
         activity.process(claim);
-        // TODO: here we should check that the status has been set to its "previous" status
-        //        - need to set up audit trail. For now, we'll check that the status has changed...
         Assert.assertNotSame(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED, claim.getStatus());
+        Assert.assertEquals(ClaimStatus.CLAIM_UNACKNOWLEDGED_ROUTED, claim.getStatus());
         Assert.assertEquals(claim.getClaimType(), ClaimType.GTA);
         Assert.assertTrue(claim.getComments().get(0).getComment().contains("Fixed Fee"));
     }
     
     @Test
-    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void testFixedFeeOriginalClaimRejectionContest() throws Throwable {
         Claim claim = new Claim();
+        claim.setId(996);
         claim.setClaimType(ClaimType.FIXED_FEE_ORIGINAL_INVOICE);
         Insurer insurer = insurerService.getInsurer(3);
         claim.setInsurer(insurer);
         claim.setStatus(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED);
-        claimService.saveClaimWithoutUpdatingLiabilityPayment(claim);
         Activity activity = activityFactory.getActivity("sendClaimGTA");
 
         activity.process(claim);
-        // TODO: here we should check that the status has been set to its "previous" status
-        //        - need to set up audit trail. For now, we'll check that the status has changed...
         Assert.assertNotSame(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED, claim.getStatus());
+        Assert.assertEquals(ClaimStatus.CLAIM_UNACKNOWLEDGED_ROUTED, claim.getStatus());
         Assert.assertEquals(claim.getClaimType(), ClaimType.GTA_ORIGINAL_INVOICE);
         Assert.assertTrue(claim.getComments().get(0).getComment().contains("Fixed Fee"));
     }
     
     @Test
-    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public void testFixedFeeSuplementaryClaimRejectionContest() throws Throwable {
         Claim claim = new Claim();
+        claim.setId(996);
         claim.setClaimType(ClaimType.FIXED_FEE_SUPPLEMENTARY_INVOICE);
         Insurer insurer = insurerService.getInsurer(3);
         claim.setInsurer(insurer);
         claim.setStatus(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED);
-        claimService.saveClaimWithoutUpdatingLiabilityPayment(claim);
         Activity activity = activityFactory.getActivity("sendClaimGTA");
 
         activity.process(claim);
-        // TODO: here we should check that the status has been set to its "previous" status
-        //        - need to set up audit trail. For now, we'll check that the status has changed...
         Assert.assertNotSame(ClaimStatus.SUBSCRIBER_CLAIM_REJECTED, claim.getStatus());
+        Assert.assertEquals(ClaimStatus.CLAIM_UNACKNOWLEDGED_ROUTED, claim.getStatus());
         Assert.assertEquals(claim.getClaimType(), ClaimType.GTA_SUPPLEMENTARY_INVOICE);
         Assert.assertTrue(claim.getComments().get(0).getComment().contains("Fixed Fee"));
     }
