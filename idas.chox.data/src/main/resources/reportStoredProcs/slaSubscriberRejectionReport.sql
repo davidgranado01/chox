a3e4f1630f15730a63baa8dafe7ecea620401b20
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-01-11 12:39:44 +0000

    Queries updated to restrict status from audit_trail table not the claim table.
    	modified:   idas.chox.data/src/main/resources/reportStoredProcs/slaFixedFeeRejectionReport.sql
    	modified:   idas.chox.data/src/main/resources/reportStoredProcs/slaSubscriberRejectionReport.sql
    
    Change-Id: Iacea6771f52b58a38e6ebd7e0b7a926d2735d906

--- idas.chox.data/src/main/resources/reportStoredProcs/slaSubscriberRejectionReport.sql
+++ idas.chox.data/src/main/resources/reportStoredProcs/slaSubscriberRejectionReport.sql
@@ -1,6 +1,3 @@
-
-DROP FUNCTION slaSubscriberRejectionReport(IN startdate TEXT, IN enddate TEXT, IN insurerid INTEGER);
-
 CREATE OR REPLACE FUNCTION slaSubscriberRejectionReport(IN startdate TEXT, IN enddate TEXT, IN insurerid INTEGER)
   RETURNS TABLE("Workgroup/Claim Owner" TEXT, "Rejection Reason" VARCHAR, "Total # rejected" BIGINT, "# rejected on day 1" BIGINT, "# rejected on day 2" BIGINT, "# rejected on day 3" BIGINT, "# rejected on day 4" BIGINT, "# rejected on day 5" BIGINT) AS
 $BODY$ 
@@ -17,7 +14,7 @@ BEGIN
 
      IF (SELECT is_workgroup_enable FROM insurer WHERE id = $3) THEN
      
-        RAISE NOTICE 'Workgroup Enabled.';
+--        RAISE NOTICE 'Workgroup Enabled.';
 
         FOR i IN 1..2 LOOP
 
@@ -54,13 +51,12 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.workgroup_id = workgroupRecord.id) OR (i=1 AND c.workgroup_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
                                     AND a.created_date between $1::DATE AND $2::DATE
-                                    AND c.status = 'SubscriberClaimRejected') AS "Total # rejected",
+                                    AND a.new_status = 'SubscriberClaimRejected') AS "Total # rejected",
 
                              (SELECT
                                     COUNT(*)
@@ -71,12 +67,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.workgroup_id = workgroupRecord.id) OR (i=1 AND c.workgroup_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 1) AS "# rejected on day 1",
 
@@ -89,12 +84,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.workgroup_id = workgroupRecord.id) OR (i=1 AND c.workgroup_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 2) AS "# rejected on day 2",
 
@@ -107,12 +101,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.workgroup_id = workgroupRecord.id) OR (i=1 AND c.workgroup_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 3) AS "# rejected on day 3",
 
@@ -125,12 +118,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.workgroup_id = workgroupRecord.id) OR (i=1 AND c.workgroup_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 4) AS "# rejected on day 4",
 
@@ -143,12 +135,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.workgroup_id = workgroupRecord.id) OR (i=1 AND c.workgroup_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 5) AS "# rejected on day 5";
 
@@ -159,7 +150,7 @@ BEGIN
      
      ELSIF (SELECT is_claim_ownership_enable FROM insurer WHERE id = $3) THEN
 
-        RAISE NOTICE 'Claim Ownership Enabled.';
+--        RAISE NOTICE 'Claim Ownership Enabled.';
 
         FOR i IN 1..2 LOOP
 
@@ -196,13 +187,12 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.claim_owner_id = claimOwnerRecord.id) OR (i=1 AND c.claim_owner_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
                                     AND a.created_date between $1::DATE AND $2::DATE
-                                    AND c.status = 'SubscriberClaimRejected') AS "Total # rejected",
+                                    AND a.new_status = 'SubscriberClaimRejected') AS "Total # rejected",
 
                              (SELECT
                                     COUNT(*)
@@ -213,12 +203,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.claim_owner_id = claimOwnerRecord.id) OR (i=1 AND c.claim_owner_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 1) AS "# rejected on day 1",
 
@@ -231,12 +220,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.claim_owner_id = claimOwnerRecord.id) OR (i=1 AND c.claim_owner_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 2) AS "# rejected on day 2",
 
@@ -249,12 +237,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.claim_owner_id = claimOwnerRecord.id) OR (i=1 AND c.claim_owner_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 3) AS "# rejected on day 3",
 
@@ -267,12 +254,11 @@ BEGIN
                                     c.insurer_id = $3
                                     AND ((i=2 AND c.claim_owner_id = claimOwnerRecord.id) OR (i=1 AND c.claim_owner_id IS NULL))
                                     AND a.claim_id = c.id
-                                    AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 4) AS "# rejected on day 4",
 
@@ -287,10 +273,10 @@ BEGIN
                                     AND a.claim_id = c.id
                                     AND c.status = a.new_status
                                     AND a.reverted = FALSE
-                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = c.status AND a1.created_date < a.created_date)
+                                    AND NOT EXISTS (SELECT * FROM audit_trail a1 WHERE a1.claim_id = c.id AND a1.new_status = a.new_status AND a1.created_date < a.created_date AND a1.reverted=FALSE)
                                     AND c.claim_type IN (7,8,9)
                                     AND ((j=1 AND a.claim_reason_of_rejection = reasonOfRejection.id) OR (j=2))
-                                    AND c.status = 'SubscriberClaimRejected'
+                                    AND a.new_status = 'SubscriberClaimRejected'
                                     AND a.created_date between $1::DATE AND $2::DATE
                                     AND ((a.created_date::date - c.created_date::date) + 1) = 5) AS "# rejected on day 5";
 
@@ -301,10 +287,9 @@ BEGIN
           
      ELSE
 
-     RAISE NOTICE 'Neither Workgroup nor Claim Ownership Enabled.';
+     RAISE NOTICE 'Neither Workgroup nor Claim Ownership Enabled - report cannot be generated.';
      
      END IF;
 END; 
 $BODY$
 LANGUAGE plpgsql;
-
