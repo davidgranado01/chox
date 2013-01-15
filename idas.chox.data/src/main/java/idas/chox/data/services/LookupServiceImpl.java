e1740cd8099052e55d3f15f2ee157659b8e42393
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-01-15 19:55:03 +0000

    Further updates to remove status field: removed from direct SQL statements and test import scripts.
    	modified:   idas.chox.data/src/main/java/idas/chox/data/services/LookupServiceImpl.java
    	modified:   idas.chox.data/src/main/java/idas/chox/data/services/SecureDataService.java
    	modified:   idas.chox.data/src/test/resources/import.sql
    	modified:   idas.chox.service/src/main/java/idas/chox/service/reports/AverageSettlementAmountReport.java
    	modified:   idas.chox.service/src/test/resources/import.sql
    
    Change-Id: I824849894e356e55eb2b250caff563a59c831273

--- idas.chox.data/src/main/java/idas/chox/data/services/LookupServiceImpl.java
+++ idas.chox.data/src/main/java/idas/chox/data/services/LookupServiceImpl.java
@@ -386,7 +386,7 @@ public class LookupServiceImpl extends SecureDataService implements LookupServic
 
             StringBuilder sb = new StringBuilder();
             sb.append("select a.id as id, a.name as name from insurer ");
-            sb.append("a inner join insurer_chorganisation b on a.id = b.insurer_id and b.status=true ");
+            sb.append("a inner join insurer_chorganisation b on a.id = b.insurer_id ");
             sb.append("where a.status=true and b.chorganisation_id=:pChorganisationId order by a.name");
 
             Map extParameters = new HashMap();
@@ -427,7 +427,7 @@ public class LookupServiceImpl extends SecureDataService implements LookupServic
 
             StringBuilder sb = new StringBuilder();
             sb.append("select a.id as id, a.name as name from chorganisation ");
-            sb.append("a inner join insurer_chorganisation b on a.id = b.chorganisation_id and b.status=true ");
+            sb.append("a inner join insurer_chorganisation b on a.id = b.chorganisation_id ");
             sb.append("where a.status=true and b.insurer_id=:pInsurerId ");
             if (excludeManualCHO) {
                 sb.append("and a.insurer_upload_only=false ");
