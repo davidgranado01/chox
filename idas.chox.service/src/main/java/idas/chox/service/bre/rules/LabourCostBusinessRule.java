1ce4dcdf97c0d6fd62eac6d87f466d70e434e72a
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-01-16 12:30:43 +0000

    Non-functional updates: code cleaning.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/bre/rules/EstimatedRepairDaysPlusBandDaysDoNotExceedHireDays.java
    	modified:   idas.chox.service/src/main/java/idas/chox/service/bre/rules/LabourCostBusinessRule.java
    
    Change-Id: I78d9ab0561ba66070ceb8ae0ab91aa45b52811e5

--- idas.chox.service/src/main/java/idas/chox/service/bre/rules/LabourCostBusinessRule.java
+++ idas.chox.service/src/main/java/idas/chox/service/bre/rules/LabourCostBusinessRule.java
@@ -40,7 +40,6 @@ public class LabourCostBusinessRule implements IBusinessRule {
 
                 if (iNumberOfHireDay > iNumberDayOfLabourCostWorthy) {
                     success = false;
-//                    narrative = "The number of hire days billed by the CHO is not relative to the number of expected hire days based on the labour information provided.";
                     narrative = "The number of hire days billed by the CHO (" + iNumberOfHireDay + " days) is not relative to the number of expected hire days (" + iNumberDayOfLabourCostWorthy + " days) based on the labour information provided.";
                     LOG.debug("LabourCostBusinessRule failed: Number of hire days {} > Number of Labour cost worthy {}", iNumberOfHireDay, iNumberDayOfLabourCostWorthy);
                 }
@@ -107,14 +106,6 @@ public class LabourCostBusinessRule implements IBusinessRule {
 
     @Override
     public String getStatusAfterFailure(boolean isTpiClaim) {
-
-        // LabourCostBusinessRule().applyToClaim(claim)) STATUS = InvoiceEscalatedToHandler;
-        // return ClaimStatus.INVOICE_ESCALATED;
-
-        // CARLSON @ 20091012
-        // return ClaimStatus.INVOICE_ESCALATED_TO_CH; 
-
-        // CARLSON @ 20091028
         if (isTpiClaim) {
             return ClaimStatus.INVOICE_ESCALATED_TO_CH;
         }
