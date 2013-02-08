82df57e52244f090cbf6661e082d6d99f1406efd
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-02-08 13:52:11 +0000

    Updated report to restrict notes to those created after invoice upload and to combine notes on supplementary invoices.
    	modified:   idas.chox.data/src/main/resources/reportStoredProcs/invoice_wip_report.sql
    
    Change-Id: Ie69404ccff5375b2afab5f633980ab21037184fb

--- idas.chox.data/src/main/resources/reportStoredProcs/invoice_wip_report.sql
+++ idas.chox.data/src/main/resources/reportStoredProcs/invoice_wip_report.sql
@@ -37,11 +37,14 @@ SELECT
    (current_date - i.created_date::DATE) + 1 AS "Time Since Invoice Upload (Days)",
    c.status AS "Current Status",
    (CURRENT_DATE - c.status_modified_date::DATE) + 1 AS "Time In Current Status (Days)",
-   (case when not exists(SELECT * FROM comment co, web_user w
-                WHERE co.claim_id = c.id 
+   (case when not exists(SELECT * FROM comment co, web_user w, claim c2
+                WHERE c2.claim_number = c.claim_number
+                  AND c.insurer_id = c2.insurer_id
+                  AND co.claim_id = c2.id 
                   AND co.created_by = w.id
                   AND co.visibility_type = 0 
                   AND co.created_by != 999
+                  AND co.created_date >= i.created_date
                   AND w.chorganisation_id IS NOT null
                   AND comment NOT LIKE ('Supplier Claims Handler is%')
                   AND comment NOT LIKE ('The claim was marked as %')
@@ -60,11 +63,14 @@ SELECT
                   AND comment NOT LIKE ('A discount of £% has been applied to the % on this invoice based on the discount contract in place.')
                   AND comment NOT LIKE ('Penalty charges have been removed from the invoice as the date from which penalty charges are calculated has been manually updated.')) then null
     else (select CURRENT_DATE - co.created_date::DATE + 1  FROM comment co
-                  WHERE co.id = (select max(co.id) from comment co, web_user w
-                WHERE co.claim_id = c.id 
+                  WHERE co.id = (select max(co.id) from comment co, web_user w, claim c2
+                WHERE c2.claim_number = c.claim_number
+                  AND c.insurer_id = c2.insurer_id
+                  AND co.claim_id = c2.id 
                   AND co.created_by = w.id
                   AND co.visibility_type = 0 
                   AND co.created_by != 999
+                  AND co.created_date >= i.created_date
                   AND w.chorganisation_id IS NOT null
                   AND comment NOT LIKE ('Supplier Claims Handler is%')
                   AND comment NOT LIKE ('The claim was marked as %')
