c0ccbfd103ea4437734f872f3f7510b73b7282b4
seenimurugan <seeni.shanmugam@sherwoodcompliance.co.uk>
2013-02-14 17:41:42 +0000

    Task Export Against CHO or Insurer with Assigned Handler report change

--- idas.chox.data/src/main/resources/reportStoredProcs/taskExportReport.sql
+++ idas.chox.data/src/main/resources/reportStoredProcs/taskExportReport.sql
@@ -1,6 +1,7 @@
 DROP FUNCTION taskExportReport(character varying, numeric, IN integer[]);
 CREATE OR REPLACE FUNCTION taskExportReport(IN org character varying, IN orgid numeric, IN integer[])
   RETURNS TABLE("Supplier Reference" character varying,
+                "Insurer Claim Number" character varying,
                 "Workgroup" character varying,
                 "Current CHOX Status" character varying,   
                 "Status Of Claim When Task Created" character varying, 
@@ -23,6 +24,7 @@ IF org ILIKE 'INS' THEN
 
    select 
     c.cho_reference as "Supplier Reference",
+    c.claim_number as "Insurer Claim Number",
     wkgp.name as "Workgroup",
     c.status as "Current CHOX Status",
     a.new_status "Status Of Claim When Task Created",
@@ -37,20 +39,20 @@ IF org ILIKE 'INS' THEN
          else 'N/A' end  as "Owner",
     wur.description as "Role Assigned To"
   from 
-    task t inner join claim c on (c.id = t.claim_id)
+    task t left outer join claim c on (c.id = t.claim_id)
            left outer join web_user w on ( w.id = t.created_by)
            left outer join web_user w2 on ( w2.id = c.claim_owner_id)
            left outer join insurer ins on (ins.id = w.insurer_id)
            left outer join chorganisation cho on (cho.id = w.chorganisation_id)
            left outer join web_user_role wur on (wur.name = t.visibility_role)
            left outer join workgroup wkgp on (wkgp.id = c.workgroup_id)
-           inner join audit_trail a on (a.claim_id = c.id)
+           left outer join audit_trail a on (a.claim_id = c.id)
   where 
     t.complete = false 
     and ((t.visibility = 2 and t.insurer is true) or (t.visibility = 3 and t.insurer is false))
     and (c.insurer_id = orgId or (c.insurer_id is null and w.insurer_id = orgId))
     and ((-1 = ANY ($3)) OR (c.claim_type = ANY ($3)))
-    and a.id = (select id from audit_trail at where at.claim_id=c.id and at.created_date = (select max(created_date) as max_created_date from audit_trail a3 where a3.claim_id = c.id and ((a3.created_date <= t.created_date and reverted = false) or (a3.created_date <= t.created_date and reverted = true and last_modified_date > t.created_date))) order by id desc limit 1)
+    and ((c is null) or (a.id = (select id from audit_trail at where at.claim_id=c.id and at.created_date = (select max(created_date) as max_created_date from audit_trail a3 where a3.claim_id = c.id and ((a3.created_date <= t.created_date and reverted = false) or (a3.created_date <= t.created_date and reverted = true and last_modified_date > t.created_date))) order by id desc limit 1)))
   order by c.cho_reference, t.created_date;
 
 
@@ -60,6 +62,7 @@ ELSIF org ILIKE 'CHO' THEN
 
     select 
     c.cho_reference as "Supplier Reference", 
+    c.claim_number as "Insurer Claim Number",
     '-' as "Workgroup",
     c.status as "Current CHOX Status", 
     a.new_status "Status Of Claim When Task Created",
@@ -73,19 +76,19 @@ ELSIF org ILIKE 'CHO' THEN
     w2.first_name || ' ' || w2.last_name as "Owner", 
     wur.description as "Role Assigned To"
   from 
-    task t inner join claim c on (c.id = t.claim_id)
+    task t left outer join claim c on (c.id = t.claim_id)
            left outer join web_user w on ( w.id = t.created_by)
            left outer join web_user w2 on ( w2.id = c.cho_claim_owner_id)
            left outer join insurer ins on (ins.id = w.insurer_id)
            left outer join chorganisation cho on (cho.id = w.chorganisation_id)
            left outer join web_user_role wur on (wur.name = t.visibility_role)
-           inner join audit_trail a on (a.claim_id = c.id)
+           left outer join audit_trail a on (a.claim_id = c.id)
   where
     t.complete = false 
     and ((t.visibility = 2 and t.insurer is false) or (t.visibility = 3 and t.insurer is true))
     and (c.chorganisation_id = orgId or (c.chorganisation_id is null and w.chorganisation_id = orgId))
     and ((-1 = ANY ($3)) OR (c.claim_type = ANY ($3)))
-    and a.id = (select id from audit_trail at where at.claim_id=c.id and at.created_date = (select max(created_date) as max_created_date from audit_trail a3 where a3.claim_id = c.id and ((a3.created_date <= t.created_date and reverted = false) or (a3.created_date <= t.created_date and reverted = true and last_modified_date > t.created_date))) order by id desc limit 1)
+    and ((c is null) or (a.id = (select id from audit_trail at where at.claim_id=c.id and at.created_date = (select max(created_date) as max_created_date from audit_trail a3 where a3.claim_id = c.id and ((a3.created_date <= t.created_date and reverted = false) or (a3.created_date <= t.created_date and reverted = true and last_modified_date > t.created_date))) order by id desc limit 1)))
     order by c.cho_reference, t.created_date;      
 
 
