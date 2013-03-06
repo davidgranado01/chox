f5618dac3636875d5c0575c38ae74e3fcc4063ff
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-03-06 09:46:31 +0000

    Minor updates to WSDL.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/security/ApplicationAccessibility.java
    	modified:   idas.chox.web/src/main/resources/wsdl/chox-webservices.wsdl
    
    Change-Id: Iede230a6e5dd83032adb096dc208f9361ae9c811

--- idas.chox.service/src/main/java/idas/chox/service/security/ApplicationAccessibility.java
+++ idas.chox.service/src/main/java/idas/chox/service/security/ApplicationAccessibility.java
@@ -30,7 +30,7 @@ public class ApplicationAccessibility {
     private AccessibilityService accessibilityService;
 
     // ***************************************
-    // Activities via buttons
+    // Activities via buttons (other activities defined in ActionPanel.java
     // ***************************************
     public static final String SWITCH_CLAIM = "SwitchClaim";
     public static final String REVERT_CLAIM = "RevertClaim";
@@ -40,7 +40,7 @@ public class ApplicationAccessibility {
     public static final String PAYMENT_NOT_RECEIVED = "PaymentNotReceived";
 
     // ***************************************
-    // FILTER
+    // FILTERS
     // ***************************************
     public static final String FILTER_REJECTED_CLAIMS = "RejectedClaims";
     public static final String FILTER_REJECTED_SUBSCRIBER_CLAIMS = "RejectedSubscriberClaims";
