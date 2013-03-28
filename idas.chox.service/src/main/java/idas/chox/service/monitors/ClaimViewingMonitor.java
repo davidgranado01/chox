fa8d6a3222c967221457c7b2e7432e31c0fcdf43
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-03-28 10:09:39 +0000

    Non-functional changes (code cleaning).
    	modified:   idas.chox.service/src/main/java/idas/chox/service/monitors/ClaimViewingMonitor.java
    	modified:   idas.chox.service/src/main/java/idas/chox/service/reports/OverviewSummaryReport.java
    
    Change-Id: Id79e7128b678b45484706c3fd5b140d8010d90ea

--- idas.chox.service/src/main/java/idas/chox/service/monitors/ClaimViewingMonitor.java
+++ idas.chox.service/src/main/java/idas/chox/service/monitors/ClaimViewingMonitor.java
@@ -3,16 +3,11 @@ package idas.chox.service.monitors;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  *
  * @author Emmanuel
  */
 public final class ClaimViewingMonitor {
-    private static final Logger LOG = LoggerFactory.getLogger(ClaimViewingMonitor.class);
-
     private static ClaimViewingMonitor instance = new ClaimViewingMonitor();
     private ConcurrentHashMap<String, ClaimViewState> claims = new ConcurrentHashMap<String, ClaimViewState>();
 
@@ -24,27 +19,25 @@ public final class ClaimViewingMonitor {
     }
 
     public List<Integer> ping(Integer claimId, String CompanyType, Integer orgId, Integer userId, long interval) {
-        String key = forStateKey(claimId, CompanyType, orgId);
+        String key = forStateKey(claimId);
 
         if (claims.containsKey(key)) {
-            LOG.debug("Claim already exists in viewing monitor: {}", key);
             ClaimViewState vs = claims.get(key);
             vs.ping(userId, interval);
         } else {
             ClaimViewState vs = new ClaimViewState(userId, interval);
             claims.putIfAbsent(key, vs);
-            LOG.debug("Claim added to viewing monitor: {}", key);
         }        
         return getWhoIsViewing(key);        
     }
 
-    private String forStateKey(Integer claimId, String CompanyType, Integer orgId) {
-//        return String.format("%d_%s_%d", claimId, CompanyType, orgId);
+    private String forStateKey(Integer claimId) {
+//        previously we used org and org type "%d_%s_%d", claimId, CompanyType, orgId
         return String.format("%d", claimId);
     }
 
     public List<Integer> getWhoIsViewing(Integer claimId, String CompanyType, Integer orgId) {
-//        String stateKey = String.format("%d_%s_%d", claimId, CompanyType, orgId);
+//        previously we used org and org type "%d_%s_%d", claimId, CompanyType, orgId
         String stateKey = String.format("%d", claimId);
         return getWhoIsViewing(stateKey);
     }
@@ -56,11 +49,9 @@ public final class ClaimViewingMonitor {
             return null;
         }
         if (!cvs.isExpired()) {
-            LOG.debug("Returning {} user ids from viewing monitor (claim being viewed).", cvs.getUserIds().size());
             return cvs.getUserIds();
         } else {
             claims.remove(key);
-            LOG.trace("Claim expired and removed from viewing monitor: {}", key);
             return null;
         }
 
@@ -68,7 +59,7 @@ public final class ClaimViewingMonitor {
     
     public boolean isClaimViewingBySomeBody(Integer claimId, String CompanyType, Integer orgId)
     {
-//        String key = String.format("%d_%s_%d", claimId, CompanyType, orgId);
+//        previously we used org and org type "%d_%s_%d", claimId, CompanyType, orgId
         String key = String.format("%d", claimId);
         
         ClaimViewState cvs = claims.get(key);
@@ -77,11 +68,9 @@ public final class ClaimViewingMonitor {
             return false;
         }
         if (!cvs.isExpired()) {
-            LOG.debug("Claim is being viewed: {}", key);
             return true;
         } else {
             claims.remove(key);
-            LOG.trace("Claim expired and removed from viewing monitor: {}", key);
             return false;
         }
     }
