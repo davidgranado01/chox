ae81b9071c488302517d0de71c6796d0071db873
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-04-10 10:59:16 +0100

    Non-functional update - added missing bracket in log message.
    	modified:   idas.chox.web/src/main/java/idas/chox/web/security/NonceInterceptor.java
    
    Change-Id: I1140f38a73c43c54d80d63e0090c447b0c1f9ec1

--- idas.chox.web/src/main/java/idas/chox/web/security/NonceInterceptor.java
+++ idas.chox.web/src/main/java/idas/chox/web/security/NonceInterceptor.java
@@ -1,6 +1,8 @@
 package idas.chox.web.security;
 
 import java.util.Map;
+import java.util.Arrays;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -17,11 +19,12 @@ import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
  * @author John
  */
 public class NonceInterceptor extends AbstractInterceptor {
+
     private static final Logger LOG = LoggerFactory.getLogger(NonceInterceptor.class);
 
     @Override
     public String intercept(ActionInvocation invocation) throws Exception {
-	final ActionContext context = invocation.getInvocationContext();
+        final ActionContext context = invocation.getInvocationContext();
 
         final HttpServletRequest request = (HttpServletRequest) context.get(StrutsStatics.HTTP_REQUEST);
         HttpSession session = request.getSession(false);
@@ -40,12 +43,13 @@ public class NonceInterceptor extends AbstractInterceptor {
                     LOG.error("No nonce found in session.");
                     return "invalid.token";
                 }
-                String sessionNonce = (String)sessionMap.get("SessionNonce");
+                String sessionNonce = (String) sessionMap.get("SessionNonce");
 
                 // Get request nonce
                 if (request.getParameter("nonce") == null) {
-                    LOG.error("No nonce found in request '{}' with sessionNonce='{}' and sessionId='{}' (with query string '{}'",
-                            new Object[]{request.getRequestURL(), sessionNonce, session.getId(), request.getQueryString()});
+                    LOG.error("No nonce found in request '{}' with sessionNonce='{}' and sessionId='{}' (with query string '{}') - parameters follow:\n{}",
+                            new Object[]{request.getRequestURL(), sessionNonce, session.getId(), request.getQueryString(), dumpParams(request)});
+                    
                     return "invalid.token";
                 }
                 String requestNonce = request.getParameter("nonce");
@@ -63,4 +67,18 @@ public class NonceInterceptor extends AbstractInterceptor {
         return invocation.invoke();
     }
 
+    public static String dumpParams(HttpServletRequest req) {
+        StringBuilder sb = new StringBuilder();
+        Set<Map.Entry<String, String[]>> entries = req.getParameterMap().entrySet();
+        for (Map.Entry<String, String[]> entry : entries) {
+            sb.append(entry.getKey())
+                    .append("=")
+                    .append(Arrays.toString(entry.getValue()))
+                    .append(", ");
+        }
+        if (sb.length() > 2) {
+            sb.setLength(sb.length() - 2);  //Removes the last comma
+        }
+        return sb.toString();
+    }
 }
