00c843b813590b179be65c6bf5a3a2225fcd9ce5
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-04-11 09:44:34 +0100

    Non-functional update: added IP address to message logged when users authenticates.
    	modified:   idas.chox.web/src/main/java/idas/chox/web/security/CustomAuthenticationSuccessHandler.java
    
    Change-Id: If74ba4fba2e014cbe72bdc77e4b3a07a8798fe9f

--- idas.chox.web/src/main/java/idas/chox/web/security/CustomAuthenticationSuccessHandler.java
+++ idas.chox.web/src/main/java/idas/chox/web/security/CustomAuthenticationSuccessHandler.java
@@ -181,7 +181,7 @@ public class CustomAuthenticationSuccessHandler extends SavedRequestAwareAuthent
             LOG.warn("Error updating users last login time: {}", ex.getMessage());
             LOG.warn("UserID: {}, lastlogin='{}' version=" + user.getVersion(), user.getId(), user.getLastLoginDate());
         }
-        LOG.info("User '{}' logged-in successfully.", user.toString());
+        LOG.info("User '{}' logged-in successfully from IP address {}.", user.toString(), request.getRemoteAddr());
         checkBrowserWarning(request, response, getDefaultTargetUrl());
         super.onAuthenticationSuccess(request, response, authentication);
     }
