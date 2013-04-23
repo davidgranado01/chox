405ed8fd3f1fe1dad3b3fa5085998668f8d9ad5d
seenimurugan <seeni.shanmugam@sherwoodcompliance.co.uk>
2013-04-23 11:42:42 +0100

    BUG#2583 - Production - Error handling improvements in scheduler email processor

--- idas.chox.web/src/main/java/idas/chox/web/scheduler/ServerConfig.java
+++ idas.chox.web/src/main/java/idas/chox/web/scheduler/ServerConfig.java
@@ -1,38 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package idas.chox.web.scheduler;
 
 import javax.servlet.ServletContext;
 import org.springframework.web.context.ServletContextAware;
 
-/**
- *
- * @author seeni
- */
 public class ServerConfig implements ServletContextAware {
 
     private ServletContext servletContext;
-//    private String serverRootUrl2;
-
-//    public String getServerRootUrl2() {
-//        return serverRootUrl2;
-//    }
-//
-//    public void setServerRootUrl2(String serverRootUrl2) {
-//        this.serverRootUrl2 = serverRootUrl2;
-//    }
 
     public ServletContext getServletContext() {
         return servletContext;
     }
-    
-
 
     @Override
     public void setServletContext(ServletContext servletContext) {
         this.servletContext = servletContext;
-//        this.serverRootUrl2 = servletContext.getResource("/").getPath();
     }
 }
