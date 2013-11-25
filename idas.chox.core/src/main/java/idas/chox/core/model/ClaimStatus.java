ebca7a4cdf3d5889f6bbe61a7a02dd80ce466d5f
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2013-11-25 12:21:59 +0100

    Implementation of todo item 7.8.4 Action For Hadlers - Manual Claims.
    	modified:   idas.chox.core/src/main/java/idas/chox/core/model/ClaimStatus.java

--- idas.chox.core/src/main/java/idas/chox/core/model/ClaimStatus.java
+++ idas.chox.core/src/main/java/idas/chox/core/model/ClaimStatus.java
@@ -73,6 +73,9 @@ public class ClaimStatus {
         handlerOutstandingStatusList.add(ClaimStatus.AWAITING_INVOICE_PAYMENT);
         handlerOutstandingStatusList.add(ClaimStatus.CLAIM_PENDING);
         handlerOutstandingStatusList.add(ClaimStatus.AWAITING_LIABILITY_RESOLUTION);
+        handlerOutstandingStatusList.add(ClaimStatus.MANUAL_INVOICE_APPROVED);
+        handlerOutstandingStatusList.add(ClaimStatus.MANUAL_INVOICE_REJECTED);
+        handlerOutstandingStatusList.add(ClaimStatus.MANUAL_INVOICE_CONTESTED);
     }
 
     public static List<String> getAwaitingLiabilityStatusList() {
@@ -225,6 +228,7 @@ public class ClaimStatus {
                 + CLAIM_UPDATE_BY_ENG + "','" + INVOICE_REF_TO_CH + "','"
                 + INVOICE_ESCALATED_TO_CH + "','" + CONTESTED_INVOICE_REF_TO_INS + "','"
                 + INVOICE_APPROVED_BY_BRE + "','" + AWAITING_INVOICE_PAYMENT + "','" 
+                + CLAIM_PENDING + "','" + AWAITING_LIABILITY_RESOLUTION + "','" 
                 + MANUAL_INVOICE_APPROVED + "','" + MANUAL_INVOICE_CONTESTED + "','"
                 + MANUAL_INVOICE_REJECTED + "'";
     }
