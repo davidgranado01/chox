66c1dfdd80101a5c32b7458b4ab3dc1855ca4015
John Dowson <John.Dowson@SherwoodCompliance.co.uk>
2014-07-15 13:22:32 +0200

    Fixed bug#2847 - 8.3.1 - Manager Users Cannot Action Claims In The 'Payments Team' Queue.
    	modified:   idas.chox.web/src/main/java/idas/chox/web/actions/FilterRecordCounterAction.java

--- idas.chox.web/src/main/java/idas/chox/web/actions/FilterRecordCounterAction.java
+++ idas.chox.web/src/main/java/idas/chox/web/actions/FilterRecordCounterAction.java
@@ -55,7 +55,11 @@ public class FilterRecordCounterAction extends BaseAction implements ModelDriven
             } catch (Exception ex) {
                 // Sometime this error happens when the user logout immediately after clicking the inbox queue but before the server sends the respons. 
                 // Error is thrown while getting the user information. Need to investigate further to see why it is not throwing exception all the times but some times.
-                LOG.error("Error setting up filter '{}': ", filter.getName(), ex);
+                if (getAuthenticatedUser() == null) {
+                    LOG.warn("Error setting up filter '{}' when authenticated user is empty: ", filter.getName(), ex);
+                } else {
+                    LOG.error("Error setting up filter '{}': ", filter.getName(), ex);
+                }
             }
             LOG.debug("    filter description: '{}'", filterViewData.getQueueNameWithCount());
             filterViewData.setQueueName(filter.getName());
