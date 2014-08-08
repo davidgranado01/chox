017c408619e1b59e0331c812ec58d32d5ba876cf
seeni <seeni.shanmugam@sherwoodts.co.uk>
2014-08-08 13:33:24 +0100

    report store procedure added.

---
+++ idas.chox.data/src/main/resources/reportStoredProcs/last_ecd_update_report.sql
@@ -0,0 +1,50 @@
+drop function last_ecd_update_report(integer, integer[], integer[]);
+
+create or replace function last_ecd_update_report
+(
+   IN insurerId integer, IN choIds integer[], IN claimTypes integer[]
+)
+returns table
+(
+   "Supplier Reference" character varying(128),
+   "CHO Name" character varying(128),
+   "Insurer Workgroup Name" character varying,
+   "Insurer Claim Owner" text,
+   "Date Claim Moved to AwaitingCarHireInfo Status" text,
+   "Created Date of the Most Recent ECD Update" text,
+   "ECD Provided" text
+)
+as $$ DECLARE 
+
+BEGIN 
+
+RETURN QUERY
+
+    SELECT
+          c.cho_reference,
+          cho.name,
+          wg.name,
+          (ch.first_name || ' ' || ch.last_name),
+          to_char(c.status_modified_date, 'dd/mm/yyyy'),
+          to_char(ecd.created_date, 'dd/mm/yyyy'),
+          to_char(ecd.ecd_date, 'dd/mm/yyyy')
+    FROM 
+          claim c INNER JOIN chorganisation cho ON c.chorganisation_id = cho.id 
+          INNER JOIN insurer ins ON c.insurer_id = ins.id
+          LEFT OUTER JOIN workgroup wg ON c.workgroup_id = wg.id
+          LEFT OUTER JOIN web_user ch ON c.claim_owner_id = ch.id
+          LEFT OUTER JOIN hire_monitoring_ecd ecd ON (ecd.claim_id = c.id AND ecd.created_date = (SELECT MAX(created_date) FROM hire_monitoring_ecd WHERE claim_id = c.id))
+    WHERE
+          ins.id = insurerId
+          AND (CASE WHEN array_length(choIds, 1) > 0 THEN c.chorganisation_id = ANY(choIds) ELSE TRUE END)
+          AND (CASE WHEN array_length(claimTypes, 1) > 0 THEN c.claim_type = ANY(claimTypes) ELSE TRUE END)
+          AND c.status ilike 'AwaitingCarHireInfo'
+          ORDER BY cho.name ASC, c.cho_reference ASC;
+END
+;
+$$ LANGUAGE plpgsql
+;
+GRANT EXECUTE ON FUNCTION last_ecd_update_report(integer, integer[], integer[]) TO chox_user;
+GRANT EXECUTE ON FUNCTION last_ecd_update_report(integer, integer[], integer[]) TO chox_mi;
+-- e.g.
+--     select * from last_ecd_update_report(20, array[]::integer[], array[]::integer[]);
