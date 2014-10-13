51fe99fadf6eee03b57082e96dbb336a55365611
John Dowson <John.Dowson@valexa.com>
2014-10-13 17:54:59 +0100

    Fixed bug#2897 Production - UI error when Claims Handler views Payment Team invoice.
    	modified:   idas.chox.web/src/main/java/idas/chox/web/actions/ClaimAction.java

--- idas.chox.web/src/main/java/idas/chox/web/actions/ClaimAction.java
+++ idas.chox.web/src/main/java/idas/chox/web/actions/ClaimAction.java
@@ -1203,7 +1203,7 @@ public class ClaimAction extends BaseAction implements ModelDriven<Claim>, Prepa
                     getAuthenticatedUser(), claim);
             LOG.debug("Access for {} os {}", action, accessRight);
             // If Invoice is with Payments Team then do not show InvoicePaymentLogged for PC
-            if (accessRight > 0 && "InvoicePaymentLogged".equals(action) 
+            if (accessRight > 0 && ("InvoicePaymentLogged".equals(action) || "UpdateManualInvoicePaid".equals(action))
                     && claim.getInsurer().isPaymentsTeamEnable() 
                     && (claim.getInvoice() == null || claim.getInvoice().isPaymentTeam())
                     && !getAuthenticatedUser().isInRoleOf(WebUserRole.ROLE_INS_MNG)
@@ -1211,7 +1211,7 @@ public class ClaimAction extends BaseAction implements ModelDriven<Claim>, Prepa
                     && !getAuthenticatedUser().isInRoleOf(WebUserRole.ROLE_CHOX_ADMIN)) {
                 // Do not show if no PC or CHOX Admin role
                 accessRight = 0;
-            } else if (accessRight > 0 && "InvoicePaymentLogged".equals(action)
+            } else if (accessRight > 0 && ("InvoicePaymentLogged".equals(action) || "UpdateManualInvoicePaid".equals(action))
                     && claim.getInsurer().isPaymentsTeamEnable()
                     && (claim.getInvoice() == null || !claim.getInvoice().isPaymentTeam())
                     && !getAuthenticatedUser().isInRoleOf(WebUserRole.ROLE_INS_CH)
