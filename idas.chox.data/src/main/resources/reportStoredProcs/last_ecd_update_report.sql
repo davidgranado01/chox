92f303e1f59ed4441f9792db77e1d75acbffc050
John Dowson <John.Dowson@valexa.com>
2014-11-11 15:58:43 +0000

    Added last modified LOU update column at request of BR/ER.
    	modified:   idas.chox.data/src/main/resources/reportStoredProcs/last_ecd_update_report.sql

--- idas.chox.data/src/main/resources/reportStoredProcs/last_ecd_update_report.sql
+++ idas.chox.data/src/main/resources/reportStoredProcs/last_ecd_update_report.sql
@@ -12,7 +12,8 @@ returns table
    "Insurer Claim Owner" text,
    "Date Claim Moved to AwaitingCarHireInfo Status" text,
    "Created Date of the Most Recent ECD Update" text,
-   "ECD Provided" text
+   "ECD Provided" text,
+   "Latest LOU Update" text
 )
 as $$ DECLARE 
 
@@ -27,12 +28,19 @@ RETURN QUERY
           (ch.first_name || ' ' || ch.last_name),
           to_char(c.status_modified_date, 'dd/mm/yyyy'),
           to_char(ecd.created_date, 'dd/mm/yyyy'),
-          to_char(ecd.ecd_date, 'dd/mm/yyyy')
+          to_char(ecd.ecd_date, 'dd/mm/yyyy'),
+          greatest(hmd.inspection_booked_date_last_modified, hmd.inspection_date_last_modified,
+                   hmd.repair_authorised_date_last_modified, hmd.repair_book_in_date_last_modified,
+                   hmd.repair_commenced_date_last_modified, hmd.repair_completion_date_last_modified,
+                   hmd.total_loss_offer_made_last_modified, hmd.total_loss_offer_accepted_last_modified,
+                   hmd.total_loss_check_issued_last_modified, hmd.total_loss_check_received_last_modified      
+          )
     FROM 
           claim c INNER JOIN chorganisation cho ON c.chorganisation_id = cho.id 
           INNER JOIN insurer ins ON c.insurer_id = ins.id
           LEFT OUTER JOIN workgroup wg ON c.workgroup_id = wg.id
           LEFT OUTER JOIN web_user ch ON c.claim_owner_id = ch.id
+          LEFT OUTER JOIN hire_monitoring_detail hmd ON c.hire_monitoring_detail_id = hmd.id
           LEFT OUTER JOIN hire_monitoring_ecd ecd ON (ecd.claim_id = c.id AND ecd.created_date = (SELECT MAX(created_date) FROM hire_monitoring_ecd WHERE claim_id = c.id))
     WHERE
           ins.id = insurerId
