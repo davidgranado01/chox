de07c3e2678d2c10f27a0139979ae7f152656d58
John Dowson <John.Dowson@valexa.com>
2015-02-11 16:45:28 +0100

    Added some null pointer checks.
    	modified:   idas.chox.core/src/main/java/idas/chox/core/util/AccessibilityHelper.java

--- idas.chox.core/src/main/java/idas/chox/core/util/AccessibilityHelper.java
+++ idas.chox.core/src/main/java/idas/chox/core/util/AccessibilityHelper.java
@@ -6,19 +6,16 @@ import idas.chox.core.model.WebUser;
 public class AccessibilityHelper {
 
     public static short isClaimEditable(boolean accessibilityWorkgroupCheck, boolean accessibilityClaimOwnerCheck, Claim claim, WebUser user) {
-
         boolean bWorkGroupFlag = true;
         boolean bOwnershipFlag = true;
 
         // ONLY INSURER USER
         // ONLY INSURER WORKGROUPCONTROL IS TRUE
         if (RoleHelper.isInsurerUser(user) && RoleHelper.isWorkgroupValidationEnabledUser(user) && accessibilityWorkgroupCheck && user.getInsurer().isClaimLocked()) {
-
             // SET TO FALSE IF CLAIM's WORKGROUP IN USER'S WORKGROUP(S)
             if (!AccessibilityHelper.isClaimWorkgroupOwnByUser(user, claim)) {
                 bWorkGroupFlag = false;
             }
-
         }
 
         // ONLY INSURER USER
@@ -35,61 +32,53 @@ public class AccessibilityHelper {
         return 1;
     }
 
+    
     public static boolean getIsClaimWorkgroupEditable(Claim claim, WebUser user) {
-
         boolean bWorkGroupFlag = true;
 
         // ONLY INSURER USER
         // ONLY INSURER WORKGROUPCONTROL IS TRUE
         if (RoleHelper.isWorkgroupValidationEnabledUser(user) && user.getInsurer().isClaimLocked()) {
-
             // SET TO FALSE IF CLAIM's WORKGROUP IN USER'S WORKGROUP(S)
             if (!isClaimWorkgroupOwnByUser(user, claim)) {
                 bWorkGroupFlag = false;
             }
-
         }
-
         return bWorkGroupFlag;
-
     }
 
-    public static boolean getIsClaimOwnershipEditable(Claim claim, WebUser user) {
 
+    public static boolean getIsClaimOwnershipEditable(Claim claim, WebUser user) {
         boolean bOwnershipFlag = true;
 
         // ONLY INSURER USER
         // ONLY INSURER OWNERSHIP IS TRUE
-        if (user.getInsurer() != null && RoleHelper.isOwnershipValidationEnabledUser(user) && user.getInsurer().isClaimLocked()) {
-
+        if (user != null && user.getInsurer() != null && RoleHelper.isOwnershipValidationEnabledUser(user) && user.getInsurer().isClaimLocked()) {
             if (!AccessibilityHelper.isClaimOwnByUser(user, claim)) {
                 bOwnershipFlag = false;
             }
-
         }
-
         return bOwnershipFlag;
-
     }
 
-    public static boolean isClaimWorkgroupOwnByUser(WebUser user, Claim claim) {
 
+    public static boolean isClaimWorkgroupOwnByUser(WebUser user, Claim claim) {
         boolean bFlag = false;
 
-        if (user.getWorkgroupIds().size() > 0 && claim.getWorkgroup() != null) {
-
+        if (user != null && user.getWorkgroupIds().size() > 0 && claim.getWorkgroup() != null) {
             if (user.getWorkgroupIds().contains(claim.getWorkgroup().getId())) {
                 bFlag = true;
             }
         }
-
         return bFlag;
     }
 
+
     public static boolean isClaimOwnByUser(WebUser user, Claim claim) {
         boolean bFlag = false;
-        if (claim.getClaimOwner() != null) {
-            if (user.getId().equals(claim.getClaimOwner().getId())) {
+
+        if (claim!= null && claim.getClaimOwner() != null) {
+            if (user != null && user.getId().equals(claim.getClaimOwner().getId())) {
                 bFlag = true;
             }
         }
