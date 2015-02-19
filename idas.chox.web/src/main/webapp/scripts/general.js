4bb86eb004fdcbeb012883306ba17fae15788cac
John Dowson <John.Dowson@valexa.com>
2015-02-19 13:56:41 +0000

    Fixed bug#2946 - Production - Copyright is 2014.
    	modified:   idas.chox.web/src/main/webapp/WEB-INF/decorators/bare.jsp
    	modified:   idas.chox.web/src/main/webapp/WEB-INF/decorators/main.jsp
    	modified:   idas.chox.web/src/main/webapp/WEB-INF/decorators/simple.jsp
    	modified:   idas.chox.web/src/main/webapp/chox_copyright.html
    	modified:   idas.chox.web/src/main/webapp/scripts/general.js

--- idas.chox.web/src/main/webapp/scripts/general.js
+++ idas.chox.web/src/main/webapp/scripts/general.js
@@ -82,7 +82,7 @@ function onOpenAbout(){
 
     var msg = "<span class='aboutProductName'>Product Name: CHOX</span><br/><br/>";
 
-    msg = msg + "<span class='acountCopyright'>Copyright Message: &copy;2014 Valexa Technologies Limited</span><br/><br/>";
+    msg = msg + "<span class='acountCopyright'>Copyright Message: &copy;2015 Valexa Technologies Limited</span><br/><br/>";
     msg = msg + "<span class='acountVersionNumber'>" + generalChoxVersion + "</span><br/><br/>";
     msg = msg + "<input type='button' value='Close' onclick='javascript:$.unblockUI();'>";
     
