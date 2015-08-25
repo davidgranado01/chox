0c6baa20e9dc6f550b51ea7608054d9783e550ea
John Dowson <John.Dowson@valexa.com>
2015-08-25 18:52:06 +0200

    id added (for sorting purposes) anf file rename (wiki report update).
    	renamed:    idas.chox.data/src/main/resources/storedProcs/get_reconstructed_audit_trail.dql -> idas.chox.data/src/main/resources/storedProcs/get_reconstructed_audit_trail.sql

--- idas.chox.data/src/main/resources/storedProcs/get_reconstructed_audit_trail.sql
+++ idas.chox.data/src/main/resources/storedProcs/get_reconstructed_audit_trail.sql
@@ -2,6 +2,7 @@ drop function get_reconstructed_audit_trail(IN claimId INT);
 
 create or replace function get_reconstructed_audit_trail(IN claimId INT)
 RETURNS TABLE (
+    id integer,
     update_date timestamp without time zone,
     original_status character varying(40),
     new_status character varying(40)
@@ -16,10 +17,10 @@ BEGIN
         select * from audit_trail where claim_id = claimId order by update_date asc, id
     LOOP
         RETURN QUERY
-        select auditTrailRecord.update_date, auditTrailRecord.original_status, auditTrailRecord.new_status;
+        select auditTrailRecord.id, auditTrailRecord.update_date, auditTrailRecord.original_status, auditTrailRecord.new_status;
         if (auditTrailRecord.reverted and auditTrailRecord.original_status != '') THEN
             RETURN QUERY
-            select auditTrailRecord.last_modified_date as update_date, auditTrailRecord.new_status as original_status,
+            select auditTrailRecord.id, auditTrailRecord.last_modified_date as update_date, auditTrailRecord.new_status as original_status,
                    auditTrailRecord.original_status as new_status;
         END IF;
     END LOOP;
