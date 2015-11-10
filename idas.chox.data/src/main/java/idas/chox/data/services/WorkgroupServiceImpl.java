97d56812032a504b53bad2f8a9eea2a64d0ac3a9
John Dowson <John.Dowson@valexa.com>
2015-11-10 16:21:10 +0100

    Fixed bug#3032 - 8.8.2 - By Existing Allocation Not Picking Up Workgroups With No Claims Assigned.
    	modified:   idas.chox.data/src/main/java/idas/chox/data/services/WorkgroupServiceImpl.java

--- idas.chox.data/src/main/java/idas/chox/data/services/WorkgroupServiceImpl.java
+++ idas.chox.data/src/main/java/idas/chox/data/services/WorkgroupServiceImpl.java
@@ -67,9 +67,9 @@ public class WorkgroupServiceImpl extends SecureDataService implements Workgroup
         DetachedCriteria criteria = DetachedCriteria.forClass(Workgroup.class);
         criteria.add(Restrictions.eq("insurer.id", insurerId));
         criteria.add(Restrictions.eq("status", true));
-        criteria.add(Restrictions.sqlRestriction("id = (select id from (select w.id, count(*) as noClaims from workgroup w, claim c "
-                + "where c.workgroup_id = w.id and c.insurer_id = " + insurerId + " and w.status = true "
-                + " and c.status not in ('ClaimClosed','ClaimRejectionAccepted','InvoiceRejectionAccepted','PaymentReceived','AwaitingLitigationOutcome','InvoicePaymentLogged','AwaitingInvoicePayment') "
+        criteria.add(Restrictions.sqlRestriction("id = (select id from (select w.id, count(*) as noClaims from workgroup w left outer join claim c on "
+                + "(w.id = c.workgroup_id and c.status not in ('ClaimClosed','ClaimRejectionAccepted','InvoiceRejectionAccepted','PaymentReceived','AwaitingLitigationOutcome','InvoicePaymentLogged','AwaitingInvoicePayment')) "
+                + "where w.insurer_id = " + insurerId + " and w.status = true "
                 + " group by w.id order by noClaims asc limit 1) tbl)"));
         
         return findByCriteria(criteria);
