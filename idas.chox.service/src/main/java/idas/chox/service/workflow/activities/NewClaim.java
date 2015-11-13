f6bc1926c9ebfa1fa72f74251167790508574322
John Dowson <John.Dowson@valexa.com>
2015-11-13 11:06:59 +0000

    Set SLA days remaining to cut-off time for new claim if sla period is 1 day.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/NewClaim.java

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/NewClaim.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/NewClaim.java
@@ -81,10 +81,18 @@ public class NewClaim extends BaseActivity {
             // For subscriber and fixed-fee claims, we need to set the initial SLA remaining fields
             if (ClaimType.isSubscriber(claim.getClaimType()) && choBand.getSubscriberSlaDays() != 0) {
                 claim.setRemainingSlaDaysInt(choBand.getSubscriberSlaDays() - 1);
-                claim.setRemainingSlaDays(String.valueOf(claim.getRemainingSlaDaysInt()));
+                if (choBand.getSubscriberSlaDays() == 1) {
+                    claim.setRemainingSlaDays(claim.getBreBand().getSubscriberTimeCutOff());
+                } else {
+                    claim.setRemainingSlaDays(String.valueOf(claim.getRemainingSlaDaysInt()));
+                }
             } else if (ClaimType.isFixedFee(claim.getClaimType()) && choBand.getFixedFeeSlaDays() != 0) {
-                claim.setRemainingSlaDaysInt(choBand.getFixedFeeSlaDays()- 1);
-                claim.setRemainingSlaDays(String.valueOf(claim.getRemainingSlaDaysInt()));
+                claim.setRemainingSlaDaysInt(choBand.getFixedFeeSlaDays() - 1);
+                if (choBand.getFixedFeeSlaDays() == 1) {
+                    claim.setRemainingSlaDays(claim.getBreBand().getFixedFeeTimeCutOff());
+                } else {
+                    claim.setRemainingSlaDays(String.valueOf(claim.getRemainingSlaDaysInt()));
+                }
             }
         }
 
