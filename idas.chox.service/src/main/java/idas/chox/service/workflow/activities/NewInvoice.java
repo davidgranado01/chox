83124701310263021ae3b6552d893e8c265c47d8
John Dowson <John.Dowson@valexa.com>
2016-01-05 18:52:57 +0100

    Tests updated + null ponter check added.
    	modified:   idas.chox.data/src/test/resources/import.sql
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/NewInvoice.java
    	modified:   idas.chox.service/src/test/resources/applicationContext-Workflow-test.xml
    	modified:   idas.chox.service/src/test/resources/applicationContext-services-test.xml
    	modified:   idas.chox.service/src/test/resources/import.sql

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/NewInvoice.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/NewInvoice.java
@@ -117,15 +117,20 @@ public class NewInvoice extends BaseActivity {
                 : (claim.getInvoice() != null && claim.getInvoice().getDateInvoiced() != null) ? claim.getInvoice().getDateInvoiced() : new Date();
         BrePenaltyBand brePenaltyBand = brePenaltyBandService.getBrePenaltyBand(claim, hireStart);
         
-        if (brePenaltyBand.getHirePeriodStartDay1() != -1){
+        // Set the penalty band to the first band window
+        // Note that this is no longer valid for insurer uploads as the 'Date Invoiced' is used to determine the penalty age,
+        // and so an insurer uploadeded invoice could fall directly into the second or third penalty window
+        // TODO: corrct for manual invoices
+        if (brePenaltyBand != null && brePenaltyBand.getHirePeriodStartDay1() != -1){
             claim.getInvoice().setPenaltyBand(brePenaltyBand.getHirePeriodStartDay1());
-        } else if (brePenaltyBand.getHirePeriodStartDay1() == -1 && brePenaltyBand.getRepairPeriodStartDay1() != -1){
+        } else if (brePenaltyBand != null && brePenaltyBand.getHirePeriodStartDay1() == -1 && brePenaltyBand.getRepairPeriodStartDay1() != -1){
             claim.getInvoice().setPenaltyBand(brePenaltyBand.getRepairPeriodStartDay1());
         } else {
             claim.getInvoice().setPenaltyBand(-1);
         }
+
         // If the first penalty band is set to use commercial rates, then turn off automatic penalties on the claim
-        if (brePenaltyBand.isUseCommercialDay1()) {
+        if (brePenaltyBand != null && brePenaltyBand.isUseCommercialDay1()) {
             claim.setAutoPenaltyChargeEnabled(false);
         }
         // Perform HPI check
