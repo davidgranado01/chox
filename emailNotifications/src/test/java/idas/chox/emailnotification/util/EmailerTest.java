d6c1db25142a068e3a693d1fd94b5db0986a3b08
Mark-Allen <mark.allen@sherwoodts.co.uk>
2016-02-12 14:53:40 +0000

    Include sleeper into emailerTest

--- emailNotifications/src/test/java/idas/chox/emailnotification/util/EmailerTest.java
+++ emailNotifications/src/test/java/idas/chox/emailnotification/util/EmailerTest.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class EmailerTest extends AbstractEmailNotificationTest {
 
     private @Autowired EmailHelper emailHelper;
-    private static final String[] recipients = { "john.dowson@valexa.com" };
+    private static final String[] recipients = { "mark.allen@valexa.com" };
 
     @Test
     public void checkWiring() {
@@ -36,8 +36,16 @@ public class EmailerTest extends AbstractEmailNotificationTest {
         Transport transport = emailHelper.getTransport();
         for (int i = 0; i < 200; i++) {
             emailHelper.postMail(transport, "Test", "TestMesage", recipients);
+            sleep20Seconds();
         }
         emailHelper.closeTransport(transport);
     }
 
+    private void sleep20Seconds() {
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
