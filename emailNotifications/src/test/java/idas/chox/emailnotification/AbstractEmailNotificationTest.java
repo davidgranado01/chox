5ffc1e4c490d8425b3f7983345f9156040f10764
John Dowson <John.Dowson@valexa.com>
2016-02-12 12:03:23 +0100

    Added a @TestExecutionListeners annotation.
    	modified:   emailNotifications/src/test/java/idas/chox/emailnotification/AbstractEmailNotificationTest.java

--- emailNotifications/src/test/java/idas/chox/emailnotification/AbstractEmailNotificationTest.java
+++ emailNotifications/src/test/java/idas/chox/emailnotification/AbstractEmailNotificationTest.java
@@ -4,12 +4,15 @@ import idas.chox.emailnotification.config.EmailNotificationConfig;
 
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = EmailNotificationConfig.class)
 @TestPropertySource("classpath:/application.test.properties")
+@TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class})
 public abstract class AbstractEmailNotificationTest {
 
 }
