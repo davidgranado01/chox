6347511b652fc43daf9190b31e6003ccd23102de
John Dowson <John.Dowson@valexa.com>
2016-02-13 16:48:38 +0100

    Email sending throttle tuning.
    	modified:   emailNotifications/src/main/java/idas/chox/emailnotification/EmailNotificationController.java
    	modified:   emailNotifications/src/main/java/idas/chox/emailnotification/notifier/AbstractNotifier.java

--- emailNotifications/src/main/java/idas/chox/emailnotification/notifier/AbstractNotifier.java
+++ emailNotifications/src/main/java/idas/chox/emailnotification/notifier/AbstractNotifier.java
@@ -108,7 +108,7 @@ public abstract class AbstractNotifier implements Notifier {
                     if (!maxRecordsShown) {
                         if (enableEmails && noProcessed % 10 == 0) {
                             try {
-                                logger.info("Sleeping for {} seconds", noProcessed);
+                                logger.info("Sleeping for {} seconds", noProcessed/2);
                                 Thread.sleep(noProcessed * 500); // Wait half a second for each email sent
                             } catch (InterruptedException e) {
                                 logger.error("Sleep interrupted: %s", e.getMessage());
