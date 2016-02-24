799c455edc8bc865e1701930495227df4b7578ad
John Dowson <John.Dowson@valexa.com>
2016-02-24 18:15:00 +0000

    Updated Processed Notifications wiki report (requested by BR).
    	renamed:    idas.chox.data/src/main/resources/reportStoredProcs/processedNotificationsReport_DLG.sql -> idas.chox.data/src/main/resources/reportStoredProcs/processedNotificationsReport-superceeded.sql
    	modified:   idas.chox.data/src/main/resources/reportStoredProcs/processedNotificationsReport.sql

--- idas.chox.data/src/main/resources/reportStoredProcs/processedNotificationsReport.sql
+++ idas.chox.data/src/main/resources/reportStoredProcs/processedNotificationsReport.sql
@@ -1,29 +1,27 @@
 DROP function processedNotificationsReport(
     IN choId INTEGER,
     IN insIds INTEGER[],
-    IN claimTypes INTEGER[],
     IN startPeriod VARCHAR,
     IN endPeriod VARCHAR);
 
 CREATE OR REPLACE FUNCTION processedNotificationsReport(
     IN choId INTEGER,
     IN insIds INTEGER[],
-    IN claimTypes INTEGER[],
     IN startPeriod VARCHAR,
     IN endPeriod VARCHAR)
 
 RETURNS TABLE("AX Unique Ref" VARCHAR,
-              "Policy No" VARCHAR,
-              "Claim No" VARCHAR,
+              "Team ID" VARCHAR,
+              "Branch / Scanning Centre" VARCHAR,
+              "Policy Number" VARCHAR,
+              "Claim Number" VARCHAR,
               "Policy Holder" TEXT,
               "Customer Registration Number" VARCHAR,
               "Date of Accident" timestamp without time zone,
-              "Time of Accident" VARCHAR,
-              "Accident Location" TEXT,
               "Accident Circumstances" TEXT,
               "Hire Firm Reference Number" VARCHAR,
               "Method of Claimants Repair / Replacement" VARCHAR,
-              "RSA to contact claimant to arrange inspection/repairs" VARCHAR,
+              "Insurer to contact claimant to arrange inspection/repairs" VARCHAR,
               "Is Claimants vehicle roadworthy after the accident" VARCHAR,
               "Hire Firm Customer" text,
               "Hire Firm Customer Address" text,
@@ -32,11 +30,13 @@ RETURNS TABLE("AX Unique Ref" VARCHAR,
               "Vehicle Location" VARCHAR,
               "Vehicle Location Details" VARCHAR,
               "Claimant Vehicle Registration" VARCHAR,
+              "Vehicle Damage" VARCHAR,
               "Injury" VARCHAR,
               "Hire Commenced On (If Applicable)" timestamp without time zone,
+              "Insurance Brand" VARCHAR,
               "Liability Status" text,
               "If wrong insurer, please provide correct info" VARCHAR,
-              "Fraud" VARCHAR,
+              "Fraud - Reason For Rejection" VARCHAR,
               "In Protocol" VARCHAR)
 AS
 
@@ -49,23 +49,25 @@ DATE_TO DATE;
 
 BEGIN
 
-DATE_FROM = $4::DATE;
-DATE_TO = $5::DATE;
+DATE_FROM = $3::DATE;
+DATE_TO = $4::DATE;
 
 RETURN QUERY
 
 SELECT ''::varchar as "AX Unique Ref",
-       tp.policy_number as "Policy No",
-       c.claim_number as "Claim No",
+       ''::varchar as "Team ID",
+       case when exists(select * from audit_trail at where at.claim_id = c.id and at.new_status='AwaitingCarHireInfo'
+          and at.created_date >=  DATE_FROM and at.created_date < DATE_TO and reverted = false ) then 'CHOX'::varchar else
+       case when c.id = 6 then 'Birmingham'::varchar else case when c.id = 26 then 'Leeds'::varchar else ''::varchar end end end as "Branch / Scanning Centre",
+       tp.policy_number as "Policy Number",
+       c.claim_number as "Claim Number",
        tp.title || ' ' || tp.first_name || ' ' || tp.last_name as "Policy Holder",
        tp.vehicle_registration as "Customer Registration Number",
        inc.date as "Date of Accident",
-       inc.time as "Time of Accident",
-       inc.location as "Accident Location",
        inc.incident_description  as "Accident Circumstances",
        c.cho_reference as "Hire Firm reference number",
        ''::varchar  as "Method of Claimants repair / replacement",
-       ''::varchar  as "RSA to contact claimant to arrange inspection/repairs",
+       ''::varchar  as "Insurer to contact claimant to arrange inspection/repairs",
        case when cust.is_usable is true then 'Yes'::varchar else case when cust.is_usable is false then 'No'::varchar else '-'::varchar end end as "Is Claimants vehicle roadworthy after the accident",
        cust.title || ' ' || cust.first_name || ' ' || cust.last_name   as "Hire Firm Customer",
        cust.address1 || ' ' || cust.address2  || ' ' || cust.address3 || ' '
@@ -75,12 +77,14 @@ SELECT ''::varchar as "AX Unique Ref",
        cust.location as "Vehicle Location",
        cust.location as "Vehicle Location Details",
        cust.vehicle_registration as "Claimant Vehicle Registration",
+       cust.damage as "Vehicle Damage",
        ''::varchar as "Injury",
        vh.rental_start as "Hire Commenced On (If applicable)",
+       tp.insurer_brand as "RBS Insurance Brand",
        getLiabilityStatusAx(c.liability_status) as "Liability status",
        (select comment from comment where claim_id=c.id and comment like 'Supporting Liability Notes%' order by id desc limit 1) as "If wrong insurer, please provide correct info",
        case when exists (select * from audit_trail at where at.claim_id=c.id and at.created_date >= DATE_FROM and at.created_date < DATE_TO
-              and new_status = 'ClaimRejected' and reverted=false) then ror.name else ''::varchar end as "Fraud",
+              and new_status = 'ClaimRejected' and reverted=false) then ror.name else ''::varchar end as "Fraud - Reason For Rejection",
        case when exists(select * from audit_trail at where at.claim_id = c.id and at.new_status='AwaitingCarHireInfo'
           and at.created_date >=  DATE_FROM and at.created_date < DATE_TO and reverted = false ) then 'Yes'::varchar else 'No'::varchar end as "In Protocol"
 FROM claim c
@@ -91,12 +95,12 @@ FROM claim c
   left outer join vehicle_hire vh on (c.vehicle_hire_id = vh.id)
   left outer join reason_of_rejection ror on (c.reason_of_rejection_id = ror.id)
 WHERE (   exists (select * from audit_trail at where at.claim_id=c.id and at.created_date >= DATE_FROM and at.created_date < DATE_TO
-              and at.new_status = 'ClaimRejected' and at.reverted=false)
+              and at.new_status = 'ClaimClosed' and at.reverted=false)
        or exists (select * from audit_trail at where at.claim_id=c.id and at.created_date >= DATE_FROM and at.created_date < DATE_TO
               and at.new_status = 'AwaitingCarHireInfo' and at.reverted=false))
   AND c.chorganisation_id = choId
   AND (case when array_length(insIds, 1) > 0 then c.insurer_id = ANY(insIds) else true end)
-  AND (case when array_length(claimTypes, 1) > 0 then c.claim_type = ANY(claimTypes) else true end);
+  AND c.claim_type in (10,14,15,16,17);
 
 END;
 $BODY$
@@ -106,7 +110,6 @@ $BODY$
 GRANT EXECUTE ON FUNCTION processedNotificationsReport(
                                               IN choId INTEGER,
                                               IN insIds INTEGER[],
-                                              IN claimTypes INTEGER[],
                                               IN startPeriod VARCHAR,
                                               IN endPeriod VARCHAR)
 TO chox_user;
@@ -114,11 +117,10 @@ TO chox_user;
 GRANT EXECUTE ON FUNCTION processedNotificationsReport(
                                               IN choId INTEGER,
                                               IN insIds INTEGER[],
-                                              IN claimTypes INTEGER[],
                                               IN startPeriod VARCHAR,
                                               IN endPeriod VARCHAR)
 TO chox_mi;
 
-/* select * from processedNotificationsReport(1132, null, array[11,12,13], '2013-06-03', '2013-06-04'); */
+/* select * from processedNotificationsReport(1125, array[26], '2016-02-23', '2016-02-24'); */
 
 
