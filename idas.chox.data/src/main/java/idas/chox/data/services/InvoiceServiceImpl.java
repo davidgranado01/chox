85fede87cb7367a9dd6e3a8b91653be63bfca901
John Dowson <John.Dowson@valexa.com>
2016-05-16 17:50:45 +0100

    Corrected possible divide-by-zero error.
    	modified:   idas.chox.data/src/main/java/idas/chox/data/services/InvoiceServiceImpl.java

--- idas.chox.data/src/main/java/idas/chox/data/services/InvoiceServiceImpl.java
+++ idas.chox.data/src/main/java/idas/chox/data/services/InvoiceServiceImpl.java
@@ -113,7 +113,11 @@ public class InvoiceServiceImpl extends SecureDataService implements InvoiceServ
             invOriginal.setExcessAmountCollectedOriginal(inv.getExcessAmountCollected());
             invOriginal.setVatAmountCollectedOriginal(inv.getVatAmountCollected());
             invOriginal.setVersion(0);
-            invOriginal.setTotalToPayOriginal(inv.getTotalToPay().multiply(inv.getFullTotalToPay().divide(inv.getTotalToPay(), 2, RoundingMode.HALF_UP)).subtract(inv.getGtaDiscount()).multiply(inv.getTotalToPay().divide(inv.getFullTotalToPay(), 2, RoundingMode.HALF_UP)).setScale(2, BigDecimal.ROUND_HALF_UP));
+            if (inv.getTotalToPay().compareTo(BigDecimal.ZERO) == 0 || inv.getFullTotalToPay().compareTo(BigDecimal.ZERO) == 0) {
+                invOriginal.setTotalToPayOriginal(inv.getFullTotalToPay().subtract(inv.getGtaDiscount()));
+            } else {
+                invOriginal.setTotalToPayOriginal(inv.getTotalToPay().multiply(inv.getFullTotalToPay().divide(inv.getTotalToPay(), 2, RoundingMode.HALF_UP)).subtract(inv.getGtaDiscount()).multiply(inv.getTotalToPay().divide(inv.getFullTotalToPay(), 2, RoundingMode.HALF_UP)).setScale(2, BigDecimal.ROUND_HALF_UP));
+            }
             invOriginal.setFullTotalToPayOriginal(inv.getFullTotalToPay().subtract(inv.getGtaDiscount()));
             invOriginal.setAdditionalDriverFeeOriginal(inv.getAdditionalDriverFee());
             invOriginal.setAdditionalDriverQtyOriginal(inv.getAdditionalDriverQty());
