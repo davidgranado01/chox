43e70b7f746677986054655f1673dc04225667e1
John Dowson <John.Dowson@valexa.com>
2016-07-20 15:54:11 +0100

    Send email to keoghs implemented.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ReferFraudCheck.java
    	modified:   idas.chox.web/src/main/resources/applicationContext-Workflow.xml

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ReferFraudCheck.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/ReferFraudCheck.java
@@ -6,6 +6,9 @@ import org.springframework.security.access.annotation.Secured;
 
 import idas.chox.core.model.Claim;
 import idas.chox.core.model.Comment;
+import idas.chox.core.util.EmailHelper;
+import java.io.UnsupportedEncodingException;
+import javax.mail.MessagingException;
 
 /**
  *
@@ -13,16 +16,77 @@ import idas.chox.core.model.Comment;
  */
 public class ReferFraudCheck extends BaseActivity {
     private static final Logger LOG = LoggerFactory.getLogger(ReferFraudCheck.class);
+    private String smtpHostName;
+    private String smtpPort;
+    private String smtpEmailUser;
+    private String smtpEmailPassword;
+    private String keoghsReceiver;
+    private String keoghsBccReceiver;
+
+    public void setSmtpHostName(String smtpHostName) {
+        this.smtpHostName = smtpHostName;
+    }
+
+    public void setSmtpPort(String smtpPort) {
+        this.smtpPort = smtpPort;
+    }
+
+    public void setSmtpEmailUser(String smtpEmailUser) {
+        this.smtpEmailUser = smtpEmailUser;
+    }
+
+    public void setSmtpEmailPassword(String smtpEmailPassword) {
+        this.smtpEmailPassword = smtpEmailPassword;
+    }
+
+    public void setKeoghsReceiver(String keoghsReceiver) {
+        this.keoghsReceiver = keoghsReceiver;
+    }
+
+    public void setKeoghsBccReceiver(String keoghsBccReceiver) {
+        this.keoghsBccReceiver = keoghsBccReceiver;
+    }
 
     
     @Override
     @Secured ({"ROLE_INS"})
     protected void doProcess(Claim claim) throws Exception {
-        LOG.info("Refer claim '{}' to Keoghs...", claim.getChoReference());
+        LOG.debug("Refer claim '{}' to Keoghs...", claim.getChoReference());
+        String subject = "CHOX Fraud Referral from " + claim.getInsurer().getName() + " on claim Supplier Reference '" + claim.getChoReference() + "'";
+        String message = "Client Batch Reference Number: " + claim.getKeoghsRequest().getClientBatchReference()
+                + "\nSupplier Reference: " + claim.getChoReference()
+                + "\nInsurer Name: " + claim.getInsurer().getName()
+                + "\nInsurer Handler Name: " + claim.getClaimOwner() == null ? "(not available)" : claim.getClaimOwner().getFullName()
+                + "\nTelephone number of Handle: " + claim.getClaimOwner() == null ? "(not available)" : claim.getClaimOwner().getTelephone()
+                + "\nInsurer Claim Number: " + claim.getClaimNumber()
+                + "\nCHO Name: " + claim.getChorganisation().getName()
+                + "\nCHO Customer Name: " + claim.getCustomer().getTitle() + " " + claim.getCustomer().getFirstName() + " " + claim.getCustomer().getLastName()
+                + "\nCHO Customer VRN: " + claim.getCustomer().getVehicleRegistration();
+
+        sendMail(subject, message);
+
         claim.setSentToKeoghs(true);
         claim.addComment(Comment.newComment(1, "This claim has been referred to Keoghs."));
-        // TODO: send email
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private void sendMail(String subject, String emailMessage) {
+        String[] receivers = keoghsReceiver != null ? keoghsReceiver.split(",") : null;
+        String[] bccReceivers = keoghsBccReceiver != null ? keoghsBccReceiver.split(",") : null;
+        LOG.debug("sending mails to receivers {} and bccreceivers {} ", receivers, bccReceivers);
+        try {
+            EmailHelper emailHelper = new EmailHelper(smtpHostName, smtpPort, smtpEmailUser, smtpEmailPassword);
+            if (bccReceivers != null && bccReceivers.length > 0) {
+                emailHelper.postMail(subject, emailMessage, receivers, bccReceivers);
+            } else {
+                emailHelper.postMail(subject, emailMessage, receivers);
+            }
+        } catch (UnsupportedEncodingException e) {
+            LOG.error("Encoding Exception thrown sending email to Keoghs with smtpHostName={}, smtpPort={}, smtpEmailUser={}, smtpEmailPassword={}: ",
+                    new Object[]{smtpHostName, smtpPort, smtpEmailUser, smtpEmailPassword, e});
+        } catch (MessagingException e) {
+            LOG.error("Messaging Exception thrown sending email to Keoghs with smtpHostName={}, smtpPort={}, smtpEmailUser={}, smtpEmailPassword={}: ",
+                    new Object[]{smtpHostName, smtpPort, smtpEmailUser, smtpEmailPassword, e});
+        }
     }
     
 }
