772aa9169d839cdbfa73584b7956ae68328a6cef
John Dowson <John.Dowson@valexa.com>
2016-12-02 14:48:26 +0100

    Partial implementation: updated front end to allow %age to be selected against rule and values passed back to server. Front end implementation therefore done, need to update model and server-side.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/workflow/activities/InvoiceSaving.java
    	modified:   idas.chox.web/src/main/webapp/jsp/invoiceSavings.jspf

--- idas.chox.service/src/main/java/idas/chox/service/workflow/activities/InvoiceSaving.java
+++ idas.chox.service/src/main/java/idas/chox/service/workflow/activities/InvoiceSaving.java
@@ -1,21 +1,26 @@
 package idas.chox.service.workflow.activities;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import idas.chox.core.model.Claim;
 import idas.chox.core.model.ClaimType;
 import idas.chox.service.workflow.ActivityFactory;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class InvoiceSaving extends BaseActivity {
 
-//    private static final Logger LOG = LoggerFactory.getLogger(InvoiceSaving.class);
+    private static final Logger LOG = LoggerFactory.getLogger(InvoiceSaving.class);
     // <editor-fold defaultstate="collapsed" desc="Member Variables">
-    private String ruleId;
+    private String rules;
     private ActivityFactory activityFactory;
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Parameter Setters">
-    public void setRuleId(String ruleId) {
-        this.ruleId = ruleId;
+    public void setRules(String rules) {
+        this.rules = rules;
     }
     // </editor-fold>
 
@@ -23,19 +28,45 @@ public class InvoiceSaving extends BaseActivity {
         this.activityFactory = activityFactory;
     }
 
+    @Override
+    protected void validate(Claim claim) throws Exception {
+        // parse rules string
+        int no50plus = 0;
+        int no25plus = 0;
+        
+        List<String> selectedRules = Arrays.asList(rules.split("\\s*,\\s*"));
+        if (selectedRules == null || selectedRules.isEmpty()) {
+            throw new Exception("At least one rule must be selected.");
+        }
+        for(String selectedRule : selectedRules) {
+            String[] tokens = selectedRule.split(":");
+            if (tokens[1].equals("3")) no50plus++;
+            if (tokens[1].equals("2")) no25plus++;
+            
+            if (no50plus > 1) {
+                throw new Exception("Cannot select more than one rule at > 50%.");
+            } else if (no50plus == 1 && no25plus > 2) {
+                throw new Exception("Cannot select more than 2 25-50% rules when one > 50% selected.");
+            } else if (no25plus > 4) {
+                throw new Exception("Cannot select more than four rules at 25-50%.");
+            }
+            LOG.debug("Selected rules: {}-{}", tokens[0], tokens[1].equals("1") ? "<25%":  tokens[1].equals("2") ? "25-50%" : " > 50%");
+        }
+        throw new Exception("Implementation not finished");
+    }
 
     @Override
     protected void doProcess(Claim claim) {
-        claim.getInvoice().setInvoiceSavingRule(ruleId);
+//        claim.getInvoice().setInvoiceSavingRule(null);
     }
 
     @Override
     protected void afterProcess(Claim claim) throws Exception {
         activityEventGenerator.generate(claim, this);
         // If this is a manual claim, we now need to change the chained activity
+        // [for non-manual claims, chained activity is acceptInvoice]
         if (ClaimType.isInsurerUpload(claim.getClaimType())) {
-            setChainActivity(activityFactory.getActivity("updateManualInvoiceAgreeQuantum"));
-
+            setChainActivity(activityFactory.getActivity("updateManualInvoiceAgreeQuantum")); 
         }
         getChainActivity().setWorkflowContext(getProcessContext());
         getChainActivity().processInBatch(claim);
