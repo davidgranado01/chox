a06a4f713d47b678f97586913c5d41423d6a14c2
John Dowson <John.Dowson@valexa.com>
2017-01-03 13:33:03 +0100

    New wiki report under CHOX-244.
    	modified:   idas.chox.data/src/main/resources/reportStoredProcs/LiabilityUpdateReport.sql

--- idas.chox.data/src/main/resources/reportStoredProcs/LiabilityUpdateReport.sql
+++ idas.chox.data/src/main/resources/reportStoredProcs/LiabilityUpdateReport.sql
@@ -9,8 +9,8 @@ returns table
    "Supplier Reference" character varying(128),
    "Previous Liability Status" text,
    "Current Liability Status" text,
-   "Liability Percentage Agreed (Insurer)" numeric(5,2),
-   "Liability Percentage Agreed (CHO)" numeric(5,2),
+   "Liability % Agreed (Insurer)" numeric(4,1),
+   "Liability % Agreed (CHO)" numeric(4,1),
    "Date of Latest Invoice Upload on Claim" date,
    "Supporting Liability Notes (Public)" character varying
 )
@@ -23,11 +23,11 @@ BEGIN
 RETURN QUERY
 
 select ins.name, c.cho_reference, '', getLiabilityStatus(c.liability_status),
-       c.percentage_liability_accepted, c.percentage_liability_cho,
+       c.percentage_liability_accepted::numeric(4,1), c.percentage_liability_cho::numeric(4,1),
        (select max(i.created_date)::date from invoice i, claim c2 where c2.customer_id=c.customer_id and c2.invoice_id=i.id),
        (select co2.comment from comment co2 where co2.claim_id=c.id
            and co2.created_date between (co.created_date - interval '5 seconds') and (co.created_date + interval '5 seconds')
-           and co2.comment like 'Supporting Liability Notes:%')
+           and co2.comment like 'Supporting Liability Note%')
 from claim c, chorganisation cho, insurer ins, comment co
 where c.insurer_id=ins.id and c.chorganisation_id=cho.id and c.id=co.claim_id
   and (co.comment like 'Liability status changed to%' or co.comment like 'Liability status changed from ''''%')
@@ -40,7 +40,7 @@ where c.insurer_id=ins.id and c.chorganisation_id=cho.id and c.id=co.claim_id
 UNION
 
 select ins.name, c.cho_reference, 'Liability Unknown', getLiabilityStatus(c.liability_status),
-       c.percentage_liability_accepted, c.percentage_liability_cho,
+       c.percentage_liability_accepted::numeric(4,1), c.percentage_liability_cho::numeric(4,1),
        (select max(i.created_date)::date from invoice i, claim c2 where c2.customer_id=c.customer_id and c2.invoice_id=i.id),
        (select co2.comment from comment co2 where co2.claim_id=c.id
            and co2.created_date between (co.created_date - interval '5 seconds') and (co.created_date + interval '5 seconds')
@@ -57,7 +57,7 @@ where c.insurer_id=ins.id and c.chorganisation_id=cho.id and c.id=co.claim_id
 UNION
 
 select ins.name, c.cho_reference, 'Liability In Negotiation', getLiabilityStatus(c.liability_status),
-       c.percentage_liability_accepted, c.percentage_liability_cho,
+       c.percentage_liability_accepted::numeric(4,1), c.percentage_liability_cho::numeric(4,1),
        (select max(i.created_date)::date from invoice i, claim c2 where c2.customer_id=c.customer_id and c2.invoice_id=i.id),
        (select co2.comment from comment co2 where co2.claim_id=c.id
            and co2.created_date between (co.created_date - interval '5 seconds') and (co.created_date + interval '5 seconds')
