3d987f1edd0c7cf081aad59cd7a36be5fd9c2e50
John Dowson <John.Dowson@valexa.com>
2017-08-08 16:53:44 +0200

    Final removed from setMessage as not liked by Spring: org.hibernate.tuple.entity.PojoEntityTuplizer - HHH000243: Setters of lazy classes cannot be final: idas.chox.core.model.Notification.message
    	modified:   idas.chox.core/src/main/java/idas/chox/core/model/Notification.java

--- idas.chox.core/src/main/java/idas/chox/core/model/Notification.java
+++ idas.chox.core/src/main/java/idas/chox/core/model/Notification.java
@@ -53,7 +53,7 @@ public class Notification extends Entity {
     /**
      * @param message the message to set
      */
-    public final void setMessage(String message) {
+    public void setMessage(String message) {
         this.message = message;
     }
 
