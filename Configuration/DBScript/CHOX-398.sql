234f0f82a3d495a5f89f365c6ed713d6f8aae436
John Dowson <John.Dowson@valexa.com>
2017-08-31 17:03:37 +0200

    SQL for LV / ERAC rates update under CHOX-398.
    	new file:   Configuration/DBScript/CHOX-398.sql

---
+++ Configuration/DBScript/CHOX-398.sql
@@ -0,0 +1,179 @@
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP1';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP1A';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP1EST';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP1ESTA';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP2';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP2A';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP2EST';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP2ESTA';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP3';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP3A';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP3EST';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'CP3ESTA';
+
+update vehicle_class_price_special_rate
+  set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F1';
+
+update vehicle_class_price_special_rate
+  set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F1A';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F1EST';
+
+update vehicle_class_price_special_rate
+  set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F1ESTA';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F2';
+
+update vehicle_class_price_special_rate
+  set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F2A';
+
+update vehicle_class_price_special_rate
+    set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F2EST';
+
+update vehicle_class_price_special_rate
+  set version = vehicle_class_price_special_rate.version + 1, price = 50.44
+from vehicle_class vc
+where vehicle_class_price_special_rate.insurer_id = 26
+  and vehicle_class_price_special_rate.chorganisation_id = 1007
+  and vehicle_class_price_special_rate.vehicle_class_id = vc.id
+  and vehicle_class_price_special_rate.start_date = '2017-06-01'::timestamp without time zone
+  and vc.name = 'F2ESTA';
