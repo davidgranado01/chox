346b10e425fd6bad241d2cd5c382295a7df6cf6b
John Dowson <John.Dowson@valexa.com>
2018-11-23 16:45:26 +0100

    Added check for empty string for clamscan location.
    	modified:   idas.chox.core/src/main/java/idas/chox/core/util/VirusCheckerUtility.java

--- idas.chox.core/src/main/java/idas/chox/core/util/VirusCheckerUtility.java
+++ idas.chox.core/src/main/java/idas/chox/core/util/VirusCheckerUtility.java
@@ -22,7 +22,7 @@ public class VirusCheckerUtility {
         Process clamscanProcess = null;
         
         // Succeed if clamscan not present
-        if (clamscanLocation == null || !Files.exists(Paths.get(clamscanLocation))) {
+        if (clamscanLocation == null || clamscanLocation.isEmpty() || !Files.exists(Paths.get(clamscanLocation))) {
             LOG.warn("Not scanning file as clamscan not found at location '{}',", clamscanLocation);
             fileDirty = false;
         } else
