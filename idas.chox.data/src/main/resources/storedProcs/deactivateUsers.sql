b6741089d53294227820ea71359b46de57612e68
John Dowson <John.Dowson@valexa.com>
2019-03-01 11:38:28 +0000

    Added additional admin/automation accounts to GDPR exclusion list.
    	modified:   idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql

--- idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
+++ idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
@@ -9,12 +9,12 @@ DECLARE
         update web_user
             set status = false, deactivated_date = now()
         where status = true and last_login_date < cutOff
-          and user_name not like 'admin@%' and id not in (999, 4391, 5454, 3757, 6410, 3893);
+          and user_name not like 'admin@%' and id not in (999, 4391, 5454, 3757, 6410, 3893, 6148);
 
         update web_user
             set status = false, deactivated_date = now()
         where status = true and last_login_date is null and created_date < cutOff
-          and user_name not like 'admin@%' and not in (999, 4391, 5454, 3757, 6410, 3893);
+          and user_name not like 'admin@%' and id not in (999, 4391, 5454, 3757, 6410, 3893, 6148);
     END;
 
 $BODY$
