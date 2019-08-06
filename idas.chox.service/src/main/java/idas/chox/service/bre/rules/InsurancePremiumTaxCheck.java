2035da316c5446da42a0bfd7c1dfda3c81af8ac1
John Dowson <John.Dowson@valexa.com>
2019-08-06 15:11:28 +0200

    Implemented CHOX-696: Update to BRE Rule 069 - Insurance Premium Tax Check.
    	modified:   idas.chox.service/src/main/java/idas/chox/service/bre/rules/InsurancePremiumTaxCheck.java

--- idas.chox.service/src/main/java/idas/chox/service/bre/rules/InsurancePremiumTaxCheck.java
+++ idas.chox.service/src/main/java/idas/chox/service/bre/rules/InsurancePremiumTaxCheck.java
@@ -3,9 +3,6 @@ package idas.chox.service.bre.rules;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import idas.chox.core.bre.IBusinessRule;
 import idas.chox.core.bre.RuleEvaluation;
 import idas.chox.core.bre.RuleEvaluationResult;
@@ -18,8 +15,6 @@ import idas.chox.core.model.ClaimType;
  */
 public class InsurancePremiumTaxCheck implements IBusinessRule {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InsurancePremiumTaxCheck.class);
-
     private String narrative = "";
 
     @Override
@@ -31,31 +26,19 @@ public class InsurancePremiumTaxCheck implements IBusinessRule {
         res.setClaimType(claim.getClaimType());
 
         if (!ClaimType.isFixedFee(claim.getClaimType())
-                && claim.getBreBand().isInsurancePremiumTaxCheck()&& claim.getInvoice().getNonStandardInsurancePremiumFee().compareTo(BigDecimal.ZERO) > 0 &&
+                && claim.getBreBand().isInsurancePremiumTaxCheck() && claim.getInvoice().getNonStandardInsurancePremiumFee().compareTo(BigDecimal.ZERO) > 0 &&
                  claim.getInvoice().getNonStandardInsurancePremiumQty() > 0) {
-
-            LOG.debug("Insurance Premium Tax Check  is active");
 
             boolean success = true;
 
             BigDecimal nonStandPremiumFee = claim.getInvoice().getNonStandardInsurancePremiumFee();
             int nonStandPremiumFeeQty = claim.getInvoice().getNonStandardInsurancePremiumQty();
 
-
-            BigDecimal standardPremium = claim.getBreBand().getStandardInsurancePremium();
             BigDecimal nonStandardPremium = claim.getBreBand().getNonStandardInsurancePremium();
 
             BigDecimal nonStandPremiumFeePerDay = nonStandPremiumFee.divide(new BigDecimal(nonStandPremiumFeeQty), 2, RoundingMode.HALF_UP);
 
-
-            LOG.debug(" non Stand Premium Fee  {}. ", nonStandPremiumFee);
-            LOG.debug(" non Stand Premium Fee Qty  {} ", nonStandPremiumFeeQty);
-            LOG.debug(" standard Premium in Admin BreBand Panel'  {}. ", standardPremium);
-            LOG.debug(" nonStandardPremium  in Admin BreBand Panel {} ", nonStandardPremium);
-            LOG.debug(" non Stand Premium Fee Per Day {} ", nonStandPremiumFeePerDay);
-
-
-            if (standardPremium != null && nonStandardPremium != null&& nonStandPremiumFeePerDay.compareTo(standardPremium) != 0 && nonStandPremiumFeePerDay.compareTo(nonStandardPremium) != 0) {
+            if (nonStandardPremium != null && nonStandPremiumFeePerDay.compareTo(nonStandardPremium) > 0) {
                 success = false;
                 narrative = "The CHO is charging £" + nonStandPremiumFeePerDay + " per day for the Insurance Premium Tax/Non Standard Risk Insurance Premium Tax, please review.";
             }
@@ -66,9 +49,8 @@ public class InsurancePremiumTaxCheck implements IBusinessRule {
 
             narrative = "";
             res.setResult(RuleEvaluationResult.RULE_SKIPPED);
-
         }
-        LOG.debug("Insurance Premium Tax Check  is end");
+        
         return res;
     }
 
