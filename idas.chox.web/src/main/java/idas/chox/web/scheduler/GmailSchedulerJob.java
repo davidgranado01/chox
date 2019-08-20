10031e4a6b78096e3d7e2cd64650f8673544a675
John Dowson <John.Dowson@valexa.com>
2019-08-20 14:11:07 +0200

    Fixed CHOX-703: Email Attachment Upload - Some attachment types are not being processed.
    	modified:   idas.chox.web/src/main/java/idas/chox/web/scheduler/GmailSchedulerJob.java

--- idas.chox.web/src/main/java/idas/chox/web/scheduler/GmailSchedulerJob.java
+++ idas.chox.web/src/main/java/idas/chox/web/scheduler/GmailSchedulerJob.java
@@ -138,7 +138,9 @@ public class GmailSchedulerJob implements Scheduler { // , ApplicationContextAwa
                 // To get the matching job, we need to remove trailing characters from the subject.
                 // All characters after the following strings (when present) should be removed: Request, Pack, Notification, Task
                 String matchSubject;
-                if (subject.toLowerCase().contains("request")) {
+                if (subject.toLowerCase().contains("attachment upload:")) {
+                    matchSubject = subject.substring(0, subject.toLowerCase().indexOf("attachment upload:") + 17);
+                } else if (subject.toLowerCase().contains("request")) {
                     matchSubject = subject.substring(0, subject.toLowerCase().indexOf("request") + 7);
                 } else if (subject.toLowerCase().contains("pack")) {
                     matchSubject = subject.substring(0, subject.toLowerCase().indexOf("pack") + 4);
@@ -146,8 +148,6 @@ public class GmailSchedulerJob implements Scheduler { // , ApplicationContextAwa
                     matchSubject = subject.substring(0, subject.toLowerCase().indexOf("notification") + 12);
                 } else if (subject.toLowerCase().contains("task")) {
                     matchSubject = subject.substring(0, subject.toLowerCase().indexOf("task") + 4);
-                } else if (subject.toLowerCase().contains("attachment upload:")) {
-                    matchSubject = subject.substring(0, subject.toLowerCase().indexOf("attachment upload:") + 17);
                 } else {
                     matchSubject = subject;
                 }
