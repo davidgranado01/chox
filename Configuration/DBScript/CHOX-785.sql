95d96e3ca10aaf2439df32130982d3b2807253e0
isabelle.lecoeuche <isabelle.lecoeuche@audatex.co.uk>
2019-12-10 09:12:59 +0000

    CHOX-785.sql Fix so that there is no error when updating ECD section as claim is in ContestedInvoiceReferredToCHO state

--- Configuration/DBScript/CHOX-785.sql
+++ Configuration/DBScript/CHOX-785.sql
@@ -6,3 +6,13 @@ SET access_right = 2
 WHERE (role = 'ROLE_CHO_MNG' OR role = 'ROLE_CHO_OPR' )
 AND accessibility_id = (SELECT accessibility.id FROM accessibility WHERE name='tab.HireMonitoring.ContestedInvoiceReferredToCHO');
 
+INSERT into accessibility(name,is_workgroup_check,is_ownership_check, check_manual_inv_workgroup_enabled, check_manual_inv_claimownership_enabled)
+     SELECT 'activity.EcdUpdate.ContestedInvoiceReferredToCHO', false, false, false, false
+WHERE NOT EXISTS
+      (Select id from accessibility WHERE name = 'activity.EcdUpdate.ContestedInvoiceReferredToCHO' AND claim_type is null) ;
+
+INSERT into accessibility(name,is_workgroup_check,is_ownership_check, check_manual_inv_workgroup_enabled, check_manual_inv_claimownership_enabled, claim_type)
+    SELECT 'activity.EcdUpdate.ContestedInvoiceReferredToCHO', false, false, false, false, 17
+WHERE NOT EXISTS
+     (Select id from accessibility WHERE name = 'activity.EcdUpdate.ContestedInvoiceReferredToCHO' AND claim_type = 17) ;
+
