8ec1d4b626c85f3eed9906be8dc5f305b908db20
isabelle.lecoeuche <isabelle.lecoeuche@audatex.co.uk>
2020-01-30 11:30:36 +0000

    VUL-6958 update list of authorized users to specification - fix typo

--- Configuration/DBScript/CHOX-767.sql
+++ Configuration/DBScript/CHOX-767.sql
@@ -20,7 +20,7 @@ DELETE FROM  gmail_scheduler_job WHERE email_subject = 'LV Reassign Claim';
 INSERT INTO gmail_scheduler_job (login_username, login_password, job_name, email_subject, authorised_user,
                                  bcc_receiver, processed_label, error_message_receiver, active, reply_to_sender,
                                  version, created_by, created_date, last_modified_by, last_modified_date)
-VALUES ('admin@lv.com', '', 'reassignClaim', 'LV Reassign Claim', 'andrew.seedhouse@lv.com',
+VALUES ('admin@lv.com', '', 'reassignClaim', 'LV Reassign Claim', 'andrew.seedhouse@lv.co.uk',
         'DL-UKBR-Audatex-penguin-reporting@audatex.com', 'Processed Reassign Claim', 'DL-UKBR-Audatex-penguin-reporting@audatex.com', 't', 't', 1, 999,
         now(), 999, now());
 
