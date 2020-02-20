13bdea8a66e6107520a65c21a6ade6a945c6c05f
bami.idowu <bami.idowu@audatex.co.uk>
2020-02-20 10:48:30 +0000

    CHOX - Deploy CHOX GTA 4.14 - Staging on PRE
    - Removing redundant alter statement

--- Configuration/DBScript/CHOX-764.sql
+++ Configuration/DBScript/CHOX-764.sql
@@ -1,7 +1,5 @@
-ALTER TABLE web_user ALTER COLUMN password TYPE character varying(37);
-UPDATE web_user set password='{MD5}'||password where password not like '{MD5}%';
-
 ALTER TABLE web_user ALTER COLUMN password TYPE character varying;
+UPDATE web_user set password='{MD5}'||password where password not like '{MD5}%';
 
 ALTER TABLE password_history ALTER COLUMN password TYPE character varying;
 UPDATE password_history set password='{MD5}'||password where password not like '{%';
