eb232e469f215e40b076ecb38d2452ea43eef507
bami.idowu <bami.idowu@audatex.co.uk>
2020-02-24 16:28:01 +0000

    QTX-7114 - Can't add MI Access role to CHO users
    - Added update statement

--- Configuration/DBScript/CHOX-764.sql
+++ Configuration/DBScript/CHOX-764.sql
@@ -1,7 +1,7 @@
 UPDATE web_user_role SET show_workgroup_disabled=TRUE, show_ownership_disabled=TRUE WHERE name='ROLE_CHO_MI';
 
 ALTER TABLE web_user ALTER COLUMN password TYPE character varying;
-UPDATE web_user set password='{MD5}'||password where password not like '{MD5}%' or password not like '{bcrypt}%';
+UPDATE web_user set password='{MD5}'||password where password not like '{MD5}%' and password not like '{bcrypt}%';
 
 ALTER TABLE password_history ALTER COLUMN password TYPE character varying;
-UPDATE password_history set password='{MD5}'||password where password not like '{MD5}%' or password not like '{bcrypt}%';
+UPDATE password_history set password='{MD5}'||password where password not like '{MD5}%' and password not like '{bcrypt}%';
