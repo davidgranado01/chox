cad4b20f6630f12a5f450450ecd48e2dbc1297b1
Joshua.Kennedy <joshua.kennedy@UKRI-NB034.local>
2020-06-02 11:59:29 +0100

    reset changed test

--- idas.chox.service/src/test/java/idas/chox/admin/AdminInsurerServiceTest.java
+++ idas.chox.service/src/test/java/idas/chox/admin/AdminInsurerServiceTest.java
@@ -314,6 +314,7 @@ public class AdminInsurerServiceTest extends BaseTest {
         breBand.setSatelliteNavigationChargeCheck(true);
         breBand.setTowBarsChargeCheck(true);
         breBand.setActualHireDaysDoesNotExceedAllowableHireDays(true);
+        breBand.setActualHireDaysDoesNotExceedTotalLossInspection(true);
         breBand.setRepairChargeCheck(true);
         breBand.setClaimHasZeroDiscountForDA(true);
         breBand.setCorrentAdminFee(true);
