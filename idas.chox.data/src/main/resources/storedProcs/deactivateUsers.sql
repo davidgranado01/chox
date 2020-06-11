22bf95cb18a86db7b8a92f3dc9bc4491548a80d9
bami.idowu <bami.idowu@audatex.co.uk>
2020-06-11 19:18:59 +0100

    CHOX-839 - CHOX - Add Users to Exclusion List

--- idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
+++ idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
@@ -9,12 +9,12 @@ DECLARE
         update web_user
             set status = false, deactivated_date = now()
         where status = true and last_login_date < cutOff
-          and user_name not like 'admin@%' and id not in (999, 4391, 5454, 3757, 6410, 3893, 6148);
+          and user_name not like 'admin@%' and id not in (999, 4391, 5454, 3757, 6410, 3893, 6148, 5002, 5, 2002, 1766, 6637, 5075, 46, 4878, 1767, 3191, 6694, 6747, 6693);
 
         update web_user
             set status = false, deactivated_date = now()
         where status = true and last_login_date is null and created_date < cutOff
-          and user_name not like 'admin@%' and id not in (999, 4391, 5454, 3757, 6410, 3893, 6148);
+          and user_name not like 'admin@%' and id not in (999, 4391, 5454, 3757, 6410, 3893, 6148, 5002, 5, 2002, 1766, 6637, 5075, 46, 4878, 1767, 3191, 6694, 6747, 6693);
     END;
 
 $BODY$
