c0aa12040a0abd9658d728a315de9cfc7e91f9d3
Idowu, Bami [UK] <bami.idowu@audatex.co.uk>
2020-06-12 15:25:41 +0100

    8.19 chox 839

--- idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
+++ idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
@@ -9,12 +9,12 @@ DECLARE
         update web_user
             set status = false, deactivated_date = now()
         where status = true and last_login_date < cutOff
-          and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','admin@dlg.com','admin@chox.com','erac_scheduler','admin@motability.com','admin@manualhelphire.com','admin@hertz.com','admin@rsa.com','admin@equity.com','admin@qbe.com','admin@tesco.com','easidrive_scheduler','admin@lv.com','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
+          and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','erac_scheduler','easidrive_scheduler','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
 
         update web_user
             set status = false, deactivated_date = now()
         where status = true and last_login_date is null and created_date < cutOff
-          and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','admin@dlg.com','admin@chox.com','erac_scheduler','admin@motability.com','admin@manualhelphire.com','admin@hertz.com','admin@rsa.com','admin@equity.com','admin@qbe.com','admin@tesco.com','easidrive_scheduler','admin@lv.com','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
+          and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','erac_scheduler','easidrive_scheduler','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
     END;
 
 $BODY$
