4c91daa4ca0ebfa32a161a4a85d00d83b7591e17
Idowu, Bami [UK] <bami.idowu@audatex.co.uk>
2020-06-15 12:31:56 +0100

    8.19 chox 839

--- idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
+++ idas.chox.data/src/main/resources/storedProcs/deactivateUsers.sql
@@ -3,20 +3,20 @@ CREATE OR REPLACE FUNCTION deactivateUsers(claimAge integer)
 $BODY$
 DECLARE
     cutOff date;
-    BEGIN
-        cutOff := now()::date  - ($1 || ' days')::interval;
+BEGIN
+    cutOff := now()::date  - ($1 || ' days')::interval;
 
-        update web_user
-            set status = false, deactivated_date = now()
-        where status = true and last_login_date < cutOff
-          and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','erac_scheduler','easidrive_scheduler','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
+    update web_user
+    set status = false, deactivated_date = now()
+    where status = true and last_login_date < cutOff
+      and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','erac_scheduler','easidrive_scheduler','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','acknowledgeClaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
 
-        update web_user
-            set status = false, deactivated_date = now()
-        where status = true and last_login_date is null and created_date < cutOff
-          and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','erac_scheduler','easidrive_scheduler','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
-    END;
+    update web_user
+    set status = false, deactivated_date = now()
+    where status = true and last_login_date is null and created_date < cutOff
+      and user_name not like 'admin@%' and user_name not in ('ecdupdate@erac.com','erac_scheduler','easidrive_scheduler','ecdupdate@aiclaimssolutions.com','paidInvoices.rsa','paidinvoices.qbe','system','acknowledgeclaims.dlg','acknowledgeClaims.dlg','erac_scheduler','claimMatcher.lv','paidInvoices.lv');
+END;
 
 $BODY$
-LANGUAGE plpgsql;
+    LANGUAGE plpgsql;
 GRANT EXECUTE on FUNCTION deactivateUsers(integer) to chox_user;
