a300c0037c30b9895094f684150bc407427dc71c
Idowu, Bami [UK] <bami.idowu@audatex.co.uk>
2020-11-13 11:13:51 +0000

    8.20 rec 9544 bi

--- idas.chox.core/src/main/java/idas/chox/core/services/VehicleClassPriceSpecialRateService.java
+++ idas.chox.core/src/main/java/idas/chox/core/services/VehicleClassPriceSpecialRateService.java
@@ -1,9 +1,11 @@
 package idas.chox.core.services;
 
+import idas.chox.core.model.VehicleClass;
+import idas.chox.core.model.VehicleClassPriceSpecialRate;
+
 import java.math.BigDecimal;
 import java.util.Date;
-
-import idas.chox.core.model.VehicleClass;
+import java.util.List;
 
 /**
  *
@@ -13,5 +15,6 @@ public interface VehicleClassPriceSpecialRateService {
 
    BigDecimal getPrice(VehicleClass vehicleClass, Date startDate, BigDecimal age, int insId, int choId) throws Exception;
    BigDecimal getPrice(VehicleClass vehicleClass, Date startDate, int insId, int choId) throws Exception;
+   List<VehicleClassPriceSpecialRate> getAllVehicleClassPriceSpecialRates();
 }
 
