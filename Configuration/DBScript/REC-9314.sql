a810cd66f62bc882236357e0952ab243db226c3b
ccppjava <zhikun.deng@valexa.com>
2021-01-08 11:27:48 +0000

    REC-9672 /  REC-9839  Dev - UI - update accessibility table to enable UI

--- Configuration/DBScript/REC-9314.sql
+++ Configuration/DBScript/REC-9314.sql
@@ -1,8 +1,24 @@
-ALTER TABLE insurer_hire_monitoring_detail
-    ADD COLUMN IF NOT EXISTS payment_type VARCHAR(10);
+DO $rename$
+BEGIN
 
-ALTER TABLE insurer_hire_monitoring_detail
-    RENAME total_loss_check_issued TO total_loss_payment_issued;
+  IF NOT EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='insurer_hire_monitoring_detail' and column_name='payment_type')
+  THEN
+    ALTER TABLE insurer_hire_monitoring_detail ADD COLUMN IF NOT EXISTS payment_type VARCHAR(10);
+  END IF;
 
-ALTER TABLE insurer_hire_monitoring_detail
-    RENAME total_loss_check_received TO total_loss_payment_received;
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='insurer_hire_monitoring_detail' and column_name='total_loss_check_issued')
+  THEN
+      ALTER TABLE "public"."insurer_hire_monitoring_detail" RENAME COLUMN "total_loss_check_issued" TO "total_loss_payment_issued";
+  END IF;
+
+  IF EXISTS(SELECT *
+    FROM information_schema.columns
+    WHERE table_name='insurer_hire_monitoring_detail' and column_name='total_loss_check_received')
+  THEN
+      ALTER TABLE "public"."insurer_hire_monitoring_detail" RENAME COLUMN "total_loss_check_received" TO "total_loss_payment_received";
+  END IF;
+END $rename$;
