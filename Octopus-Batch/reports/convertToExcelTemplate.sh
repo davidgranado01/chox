2fed777534cb19bb1b5d56b057b0fe8ead0e13ee
luis.valero <luis.valero@solera.com>
2021-07-07 09:59:20 +0200

    Extracted emails to variables of Octopus

--- Octopus-Batch/reports/convertToExcelTemplate.sh
+++ Octopus-Batch/reports/convertToExcelTemplate.sh
@@ -9,6 +9,12 @@ export PATH=${JAVA_HOME}/bin:/bin:${PATH}
 MODE=#{MODE} # EXPORT - send emails and transfer files, NOEMAIL - rports generated but not emailed but sftp ok, NOEXPORT - reports generated but not delivered via sftp or email
 DELAY_SEND_EMAIL_REPORTS=#{DELAY_SEND_EMAIL_REPORTS}
 
+#BCC/RECIPIENTS
+RECIPIENTS_CTET_ACCIDENTEXCHANGE_PROCESS_NOTIFICATION_0 = #{RECIPIENTS_CTET_ACCIDENTEXCHANGE_PROCESS_NOTIFICATION_0}
+RECIPIENTS_CTET_ACCIDENTEXCHANGE_PROCESS_NOTIFICATION_1 = #{RECIPIENTS_CTET_ACCIDENTEXCHANGE_PROCESS_NOTIFICATION_1}
+BCC_RECIPIENT_GENERAL = #{BCC_RECIPIENT_GENERAL}
+RECIPIENTS_DEV_TEAM_EMAIL = #{RECIPIENTS_DEV_TEAM_EMAIL}
+
 function getPassword {
     REPORT_NAME=$1
     case "$REPORT_NAME" in
@@ -42,9 +48,11 @@ function getEmailReceivers {
     case "$REPORT_NAME" in
         "AccidentExchange-Processed_Notifications_Report_"* )
 	    ACTIVE=1
-            EMAIL_RECEIVERS=igp.unit@accidentexchange.com,kmartin@accidentexchange.com,ymiah@accidentexchange.com,proche@accidentexchange.com;;
+            #EMAIL_RECEIVERS=igp.unit@accidentexchange.com,kmartin@accidentexchange.com,ymiah@accidentexchange.com,proche@accidentexchange.com;;
+            EMAIL_RECEIVERS=RECIPIENTS_CTET_ACCIDENTEXCHANGE_PROCESS_NOTIFICATION_0;;
         *)
-            EMAIL_RECEIVERS=unknown@nowhere.com;;
+            #EMAIL_RECEIVERS=unknown@nowhere.com;;
+            EMAIL_RECEIVERS=RECIPIENTS_CTET_ACCIDENTEXCHANGE_PROCESS_NOTIFICATION_1;;
     esac
 
 }
@@ -57,8 +65,10 @@ then
     exit 1
 fi
 
-BCC_RECIPIENTS=MichaelPaul.Kemp@valexa.com,Robert.Hon@audatex.co.uk
-RECIPIENTS=DL-UKBR-Audatex-penguin-reporting@audatex.com
+#BCC_RECIPIENTS=MichaelPaul.Kemp@valexa.com,Robert.Hon@audatex.co.uk
+BCC_RECIPIENTS=BCC_RECIPIENT_GENERAL
+#RECIPIENTS=DL-UKBR-Audatex-penguin-reporting@audatex.com
+RECIPIENTS=RECIPIENTS_DEV_TEAM_EMAIL
 
 REPORT_DIR=$1
 PROCESSED_DIR=$2
