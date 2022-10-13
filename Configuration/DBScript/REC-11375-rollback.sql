3c1376708f030104ad429263f0e5ca2873d2e093
ABiswas <ankit.biswas@solera.com>
2022-10-13 14:49:49 +0530

    rollback script for update supplier reference feature

---
+++ Configuration/DBScript/REC-11375-rollback.sql
@@ -0,0 +1,3 @@
+DELETE from accessibility_item where role='ROLE_CHO_MNG' and accessibility_id in (select id from accessibility where name like 'extraAction.updateSupplierReferenceNumber.%');
+
+DELETE from accessibility_item where role='ROLE_CHO_MNG' and accessibility_id in (select id from accessibility where name like 'activity.UpdateSupplierReference.%');
