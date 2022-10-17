cb35cf3b66d0bcd158bfd3bc31675e28ac847692
Ankit.Biswas <ankit.biswas@solera.com>
2022-10-17 17:01:53 +0530

    feat: changing the manifest file to update database changes

--- Configuration/DBScript/REC-11375-rollback.sql
+++ Configuration/DBScript/REC-11375-rollback.sql
@@ -1,3 +1,8 @@
+DO $pendingRollback$
+BEGIN
+
 DELETE from accessibility_item where role='ROLE_CHO_MNG' and accessibility_id in (select id from accessibility where name like 'extraAction.updateSupplierReferenceNumber.%');
 
 DELETE from accessibility_item where role='ROLE_CHO_MNG' and accessibility_id in (select id from accessibility where name like 'activity.UpdateSupplierReference.%');
+
+END $pendingRollback$;
